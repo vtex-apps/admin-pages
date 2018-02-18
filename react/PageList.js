c42b806382c104885154aab7f75679928c5c5f4d
amoreira <anderson.moreira@vtex.com.br>
2018-02-18 20:17:30 -0300

    List pages

--- react/PageList.js
+++ react/PageList.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import {graphql} from 'react-apollo'
 import PagesQuery from './queries/Pages.graphql'
 
+import {Link} from 'render'
+
 // eslint-disable-next-line
 class PageList extends Component {
   static propTypes = {
@@ -15,9 +17,38 @@ class PageList extends Component {
 
     const pageList = !children && (
       <div>
-        <h3>My Pages</h3>
-        {loading && 'loading'}
-        {!loading && !children && JSON.stringify(pages)}
+        <h2>My Pages</h2>
+          {loading && 'loading'}
+          {
+            !loading && !children
+            ? <table className="collapse">
+                <tbody>
+                  <tr className="striped--near-white ">
+                    <th className="pv2 ph3 tl f6 fw6 ttu">
+                      Name
+                    </th>
+                    <th className="tl f6 ttu fw6 pv2 ph3">
+                      Path
+                    </th>
+                  </tr>
+                {
+                  pages.map(page =>
+                    <tr className="striped--near-white" key={page.name}>
+                      <td className="pv2 ph3" >
+                        {page.name}
+                      </td>
+                      <td className="pv2 ph3">
+                        <Link to={`pages/page${page.path}`} className="f4 fw6 db rebel-pink no-underline underline-hover">
+                          {page.path}
+                        </Link>
+                      </td>
+                    </tr>
+                  )
+                }
+                </tbody>
+              </table>
+            : null
+          }
       </div>
     )
 
