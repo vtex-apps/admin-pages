58f056e4fa8edca36c5b16011ecf3557089f7770
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-02-19 10:55:54 -0300

    Improve layout of 'My Pages'

--- react/PageList.js
+++ react/PageList.js
@@ -5,6 +5,7 @@ import PagesQuery from './queries/Pages.graphql'
 
 import {Link} from 'render'
 import Button from '@vtex/styleguide/lib/Button'
+import shareIcon from './images/share-26.svg'
 
 // eslint-disable-next-line
 class PageList extends Component {
@@ -17,32 +18,42 @@ class PageList extends Component {
     const {data: {loading, pages}, children} = this.props
 
     const pageList = !children && (
-      <div>
-        <h2>My Pages</h2>
+      <div className="ph5 mw7 mr-auto ml-auto mv6">
+        <div className="flex justify-between items-center mb4">
+          <h1>My Pages</h1>
+          <div>
+            <Link to="pages/page/new">
+              <Button primary>New page</Button>
+            </Link>
+          </div>
+        </div>
           {loading && 'loading'}
           {
             !loading && !children
-            ? <table className="collapse">
+            ? <table className="collapse w-100">
               <tbody>
-                <tr className="striped--near-white ">
-                  <th className="pv2 ph3 tl f6 fw6 ttu">
+                <tr className="striped--near-white">
+                  <th className="pv4 ph4 tl f6 fw6 ttu">
                     Name
                   </th>
                   <th className="tl f6 ttu fw6 pv2 ph3">
                     Path
                   </th>
+                  <th className="tl f6 ttu fw6 pv2 ph3">
+                  </th>
                 </tr>
                 {
                   pages.map(page => (
                     <tr className="striped--near-white" key={page.name}>
-                      <td className="pv2 ph3" >
+                      <td className="pv5 ph4" >
                         <Link to={`/admin/pages/page/${page.name}`} className="rebel-pink no-underline underline-hover">
                           {page.name}
                         </Link>
                       </td>
                       <td className="pv2 ph3">
-                        {page.path} <a href={page.path} className="rebel-pink no-underline underline-hover">(view)</a>
+                        {page.path}
                       </td>
+                      <td><a href={page.path} className="rebel-pink no-underline underline-hover ph4"><div><img src={shareIcon} /></div></a></td>
                     </tr>
                   ))
                 }
@@ -50,11 +61,6 @@ class PageList extends Component {
             </table>
             : null
           }
-        <div className="pv4 pl50">
-          <Link to="pages/page/new">
-            <Button>New page</Button>
-          </Link>
-        </div>
       </div>
     )
 
