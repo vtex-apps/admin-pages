ae6e8b49262ae8ef6e20baf7f597be09f983785c
Guilherme Rodrigues <gadr90@gmail.com>
2018-02-22 08:56:25 -0300

    Only show empty extension points in edit mode

--- react/EmptyExtensionPoint.js
+++ react/EmptyExtensionPoint.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
 // eslint-disable-next-line
 class EmptyExtensionPoint extends Component {
+  static contextTypes = {
+    editMode: PropTypes.bool,
+  }
+
   render() {
+    const {editMode} = this.context
+    const className = `${editMode ? 'ba b--silver b--dashed mv6 pv6 tc' : 'dn'}`
+
     return (
-      <div className="ba b--silver b--dashed mv6 pv6 tc">
+      <div className={className}>
         <div className="fw3 f3">This is an empty component</div>
         <div className="fw7 f6 pt4">Click to edit it</div>
       </div>
