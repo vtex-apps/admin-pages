d38906ba8bcf6b0d5c84ded11d370d1c7b353af4
Gabriel Galc <gabriel.galc@vtex.com.br>
2018-03-02 00:15:35 -0300

    Improve form layout on both mobile and desktop

--- react/components/EditToggle.js
+++ react/components/EditToggle.js
@@ -2,6 +2,8 @@ import Toggle from '@vtex/styleguide/lib/Toggle'
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
+import '../theme.css'
+
 // eslint-disable-next-line
 export default class EditToggle extends Component {
   static propTypes = {
@@ -16,14 +18,14 @@ export default class EditToggle extends Component {
 
   render() {
     const {editTreePath, editMode} = this.props
-    if (editTreePath) {
-      return null
-    }
     return (
-      <div className="fixed z-999 bg-white br2 shadow-4 pa5" style={{top: '30px', right: '30px'}}>
+      <div className="fixed z-999 shadow-4 bg-white flex justify-between w-100 pv3 ph3 pv5-ns ph5-ns top-0">
+        <div className="f5 self-center">
+          article-example
+        </div>
         <div className="flex items-center">
           <label htmlFor="toggle1" className="pr4 pl2">Edit Mode</label>
-          <Toggle semantic id="toggle1" checked={editMode} onClick={this.handleClick} />
+          <Toggle id="toggle1" checked={editMode} onClick={this.handleClick} />
         </div>
       </div>
     )
