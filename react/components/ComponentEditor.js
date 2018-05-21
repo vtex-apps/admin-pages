2b6a7d1ff870e9e03fd5347ce815cf26ca503758
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-05-21 10:47:03 -0300

    Fix eslint errors

--- react/components/ComponentEditor.js
+++ react/components/ComponentEditor.js
@@ -4,7 +4,7 @@ import { createPortal } from 'react-dom'
 import { compose, graphql } from 'react-apollo'
 import Form from 'react-jsonschema-form'
 import PropTypes from 'prop-types'
-import { has, find, pick, map, prop, pickBy, reduce, filter, keys, merge } from 'ramda'
+import { has, find, map, prop, pickBy, reduce, filter, keys, merge } from 'ramda'
 
 import SaveExtension from '../queries/SaveExtension.graphql'
 import AvailableComponents from '../queries/AvailableComponents.graphql'
