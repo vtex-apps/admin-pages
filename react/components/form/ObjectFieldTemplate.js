84b0d237d75b32cb769869f1c231de3f3e2c9750
Alan Carpilovsky <alancarpilovsky@gmail.com>
2018-06-13 15:18:51 -0300

    Remove description from object fields

--- react/components/form/ObjectFieldTemplate.js
+++ react/components/form/ObjectFieldTemplate.js
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types'
 import React, { Fragment } from 'react'
 
 const ObjectFieldTemplate = ({
-  description,
   idSchema: { $id: id },
   properties,
   required,
@@ -15,20 +14,17 @@ const ObjectFieldTemplate = ({
         {required && '*'}
       </label>
     )}
-    {description && <div className="f6 gray">{description}</div>}
     {properties.map(property => property.content)}
   </Fragment>
 )
 
 ObjectFieldTemplate.defaultProps = {
-  description: '',
   properties: [],
   required: false,
   title: '',
 }
 
 ObjectFieldTemplate.propTypes = {
-  description: PropTypes.string,
   idSchema: PropTypes.object.isRequired,
   properties: PropTypes.array,
   required: PropTypes.bool,
