0e2bc18b24b8b6d95efe8c6aa87e49c49e27bf9f
Alan Carpilovsky <alancarpilovsky@gmail.com>
2018-06-13 15:17:24 -0300

    Move object logic to its template and refactor

--- react/components/form/FieldTemplate.js
+++ react/components/form/FieldTemplate.js
@@ -1,64 +1,31 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React from 'react'
 
-function Label(props) {
-  const { label, required, id, type } = props
-  if (!label) {
-    return <div />
-  }
-  return (
-    <label className="control-label f6 db gray" htmlFor={id}>
-      {label}
-      {required && <span className="required">*</span>}
-    </label>
-  )
-}
-
-Label.propTypes = {
-  id: PropTypes.string,
-  label: PropTypes.string,
-  required: PropTypes.bool,
-}
-
-export default function FieldTemplate(props) {
-  const {
-    id,
-    classNames,
-    label,
-    children,
-    errors,
-    help,
-    description,
-    hidden,
-    required,
-    schema,
-    rawErrors,
-  } = props
-
+const FieldTemplate = ({
+  children,
+  classNames,
+  hidden,
+}) => {
   if (hidden) {
     return children
   }
 
   return (
     <div className={`${classNames} w-100`}>
-      {schema.type === 'object' && <Label label={label} required={required} id={id} />}
-      {schema.type === 'object' && description}
       {children}
-      {help}
     </div>
   )
 }
 
-FieldTemplate.propTypes = {
-  id: PropTypes.string,
-  classNames: PropTypes.string,
-  label: PropTypes.string,
-  help: PropTypes.element,
-  required: PropTypes.bool,
-  description: PropTypes.element,
-  errors: PropTypes.object,
-  children: PropTypes.element,
-  displayLabel: PropTypes.bool,
-  hidden: PropTypes.bool,
-  rawErrors: PropTypes.arrayOf(PropTypes.string),
+FieldTemplate.defaultProps = {
+  classNames: '',
+  hidden: false,
 }
+
+FieldTemplate.propTypes = {
+  children: PropTypes.element,
+  classNames: PropTypes.string,
+  hidden: PropTypes.bool,
+}
+
+export default FieldTemplate
--- react/components/form/ObjectFieldTemplate.js
+++ react/components/form/ObjectFieldTemplate.js
@@ -1,14 +1,38 @@
-import React from 'react'
 import PropTypes from 'prop-types'
+import React, { Fragment } from 'react'
 
-export default function ObjectFieldTemplate(props) {
-  return (
-    <div>
-      {props.properties.map(prop => prop.content)}
-    </div>
-  )
+const ObjectFieldTemplate = ({
+  description,
+  idSchema: { $id: id },
+  properties,
+  required,
+  title,
+}) => (
+  <Fragment>
+    {title && (
+      <label className="db f6 gray" htmlFor={id}>
+        {title}
+        {required && '*'}
+      </label>
+    )}
+    {description && <div className="f6 gray">{description}</div>}
+    {properties.map(property => property.content)}
+  </Fragment>
+)
+
+ObjectFieldTemplate.defaultProps = {
+  description: '',
+  properties: [],
+  required: false,
+  title: '',
 }
 
 ObjectFieldTemplate.propTypes = {
+  description: PropTypes.string,
+  idSchema: PropTypes.object.isRequired,
   properties: PropTypes.array,
+  required: PropTypes.bool,
+  title: PropTypes.string,
 }
+
+export default ObjectFieldTemplate
