d7407f8419337c09f7bb54fc3abb085f1168faa5
Lucas Bebber <lucasbbebber@gmail.com>
2018-07-08 21:22:36 -0300

    Add ArrayFieldTemplate buttons

--- react/components/form/ArrayFieldTemplate.js
+++ react/components/form/ArrayFieldTemplate.js
@@ -3,8 +3,36 @@ import React, { Fragment } from 'react'
 
 const ArrayFieldTemplate = (props)=>( 
     <div>
-      {props.items.map(element => element.children)}
-      {props.canAdd && <button type="button" onClick={props.onAddClick}></button>}
+      {console.log(props)}
+      {props.items.map(element => (
+        <React.Fragment>
+          {element.children}
+          {element.hasMoveDown && (
+            <button
+              onClick={element.onReorderClick(
+                element.index,
+                element.index + 1
+              )}>
+              Down
+            </button>
+          )}
+          {element.hasMoveUp && (
+            <button
+              onClick={element.onReorderClick(
+                element.index,
+                element.index - 1
+              )}>
+              Up
+            </button>
+          )}
+          {element.hasRemove && (
+            <button type="button" onClick={element.onDropIndexClick(element.index)}>
+              Remover
+            </button>)}
+          <hr/>
+        </React.Fragment>
+      ))}
+      {props.canAdd && <button type="button" onClick={props.onAddClick}>Add banner</button>}
     </div>
 )
 
