300991b23bfb8aceecaf52c8352bc95171fc1e18
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-07-10 20:07:15 -0300

    Move to classes and fix design issues

--- react/components/form/ArrayFieldTemplateItem.js
+++ react/components/form/ArrayFieldTemplateItem.js
@@ -1,6 +1,11 @@
 import PropTypes from 'prop-types'
-import React, { Component, Fragment } from 'react'
-import { Button, IconArrowUp, IconArrowDown, IconDelete, IconCaretUp, IconCaretDown } from 'vtex.styleguide'
+import React, { Component } from 'react'
+import { IconArrowUp, IconArrowDown, IconDelete } from 'vtex.styleguide'
+
+const stopPropagation = fn => e => {
+  e.stopPropagation()
+  return fn(e)
+}
 
 export default class ArrayFieldTemplateItem extends Component {
   static propTypes = {
@@ -17,10 +22,6 @@ export default class ArrayFieldTemplateItem extends Component {
     onClose: PropTypes.func,
   }
 
-  state = {
-    isHovering: false,
-  }
-
   container = React.createRef()
 
   handleLabelClick = () => {
@@ -31,18 +32,6 @@ export default class ArrayFieldTemplateItem extends Component {
     } else {
       onOpen()
     }
-  }
-
-  handleMouseEnter = () => {
-    this.setState({
-      isHovering: true,
-    })
-  }
-
-  handleMouseLeave = () => {
-    this.setState({
-      isHovering: false,
-    })
   }
 
   render() {
@@ -56,7 +45,6 @@ export default class ArrayFieldTemplateItem extends Component {
       onDropIndexClick,
       isOpen,
     } = this.props
-    const { isHovering } = this.state
 
     return (
       <div
@@ -64,87 +52,40 @@ export default class ArrayFieldTemplateItem extends Component {
         style={{
           transition: 'height 300ms ease-in-out, opacity 150ms ease-in-out',
         }}
-        onMouseEnter={this.handleMouseEnter}
-        onMouseLeave={this.handleMouseLeave}
       >
-        <div className="flex justify-between" style={{ minHeight: '40px' }}>
+        <div className="accordion-label" onClick={this.handleLabelClick}>
           <div className="flex items-center">
-            <span
-              className="f6"
-              style={{
-                padding: '3px 8px 3px 8px',
-                fontColor: '#727273',
-                fontWeight: 'bold',
-              }}
-            >
+            <span className="f6 accordion-label-title accordion-label-title--index">
               {index + 1}
             </span>
-            <label
-              className="f6"
-              style={{
-                padding: '3px 8px 3px 8px',
-                font: '8px #727273',
-                fontWeight: 'bold',
-              }}
-            >
+            <label className="f6 accordion-label-title">
               {children.props.schema.title}
             </label>
           </div>
-          <div className="flex items-center">
-            {isHovering && (
-              <Fragment>
-                {hasMoveDown && <button
-                  style={{
-                    padding: '3px',
-                    background: 'none',
-                    border: 'none',
-                    cursor: 'pointer',
-                  }}
-                  onClick={onReorderClick(
-                    index,
-                    index + 1
-                  )}>
-                  <IconArrowDown size="12" color="#969799" />
-                </button>}
-                {hasMoveUp && <button
-                  style={{
-                    padding: '3px',
-                    background: 'none',
-                    border: 'none',
-                    cursor: 'pointer',
-                  }}
-                  onClick={onReorderClick(
-                    index,
-                    index - 1
-                  )}>
-                  <IconArrowUp size="12" color="#969799" />
-                </button>}
-                {hasRemove && (
-                  <Button
-                    size="small"
-                    variation="tertiary"
-                    onClick={onDropIndexClick(index)}
-                  >
-                    <IconDelete size="15" color="#969799" />
-                  </Button>
-                )}
-              </Fragment>
+          <div className="flex items-center accordion-label-buttons">
+            {hasMoveUp && <button
+              className="accordion-icon-button accordion-icon-button--up"
+              onClick={stopPropagation(onReorderClick(
+                index,
+                index - 1
+              ))}>
+              <IconArrowUp size="12" color="#969799" />
+            </button>}
+            {hasMoveDown && <button
+              className="accordion-icon-button accordion-icon-button--down"
+              onClick={stopPropagation(
+                onReorderClick(index, index + 1)
+              )}>
+              <IconArrowDown size="12" color="#969799" />
+            </button>}
+            {hasRemove && (
+              <button
+                className="accordion-icon-button accordion-icon-button--remove"
+                onClick={stopPropagation(onDropIndexClick(index))}
+              >
+                <IconDelete size="15" color="#969799" />
+              </button>
             )}
-            <span
-              onClick={this.handleLabelClick}
-              style={{
-                padding: '3px',
-                background: 'none',
-                border: 'none',
-                cursor: 'pointer',
-              }}
-            >
-              {isOpen ? (
-                <IconCaretUp size="12" color="#D8D8D8" />
-              ) : (
-                <IconCaretDown size="12" color="#D8D8D8" />
-              )}
-            </span>
           </div>
         </div>
         {isOpen && children}
