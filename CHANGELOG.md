aff69514540cc45299c89190ace20aa3cee61f3c
Vitória Heliane <vitoriaheliane@gmail.com>
2018-07-18 08:56:21 -0300

    Remove console errors (#54)
    * Fix svg icons attributes name
    
    * Update changelog
    
    * Release v1.12.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+## [1.12.3] - 2018-07-18
+### Fixed
+- Attributes name in the svg icon.
+
 ## [1.12.2] - 2018-07-17
 ### Fixed
 - Fix cancel bug in edit mode
--- react/images/PathMinus.js
+++ react/images/PathMinus.js
@@ -5,15 +5,15 @@ class PathMinus extends Component {
   render() {
     return (
       <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 2V0H2" transform="translate(1 6)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M0 2.031V0" transform="translate(1 10)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M2 2H0V0" transform="translate(1 14)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M2 0H0" transform="translate(5 16)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M2 0V2H0" transform="translate(9 14)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M0 0V2" transform="translate(11 10)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M0 0H2V2" transform="translate(9 6)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M0 0H2" transform="translate(5 6)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M0 3V0H10V10H7" transform="translate(6 1)" stroke="#727273" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M0 2V0H2" transform="translate(1 6)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M0 2.031V0" transform="translate(1 10)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M2 2H0V0" transform="translate(1 14)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M2 0H0" transform="translate(5 16)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M2 0V2H0" transform="translate(9 14)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M0 0V2" transform="translate(11 10)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M0 0H2V2" transform="translate(9 6)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M0 0H2" transform="translate(5 6)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
+        <path d="M0 3V0H10V10H7" transform="translate(6 1)" stroke="#727273" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
       </svg>
     )
   }
