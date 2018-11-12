5e454da6cf5ef36a61587a0a5f7106f96a63d39f
Thiago Murakami <thiago.murakami@gmail.com>
2018-11-12 19:12:19 -0200

    Add styleguide exports

--- react/typings/vtex.styleguide.d.ts
+++ react/typings/vtex.styleguide.d.ts
@@ -8,10 +8,15 @@ declare module 'vtex.styleguide' {
   export const Dropdown: ReactElement
   export const EmptyState: ReactElement
   export const IconArrowBack: ReactElement
+  export const IconEdit: ReactElement
   export const Input: ReactElement
+  export const Modal: ReactElement
+  export const PageHeader: ReactElement
   export const Pagination: ReactElement
   export const Radio: ReactElement
   export const Spinner: ReactElement
+  export const Tab: ReactElement
   export const Table: ReactElement
+  export const Tabs: ReactElement
   export const Toggle: ReactElement
 }
