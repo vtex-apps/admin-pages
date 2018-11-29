a1a0380e37fafe8ed0c46c9406ab0634a4e53fae
Alan Carpilovsky <alancarpilovsky@gmail.com>
2018-11-29 14:09:41 -0200

    Export component/configuration editor header

--- react/components/EditorContainer/Sidebar/ComponentEditor/index.tsx
+++ react/components/EditorContainer/Sidebar/ComponentEditor/index.tsx
@@ -1,5 +1,5 @@
 import { JSONSchema6 } from 'json-schema'
-import React from 'react'
+import React, { Fragment } from 'react'
 import { injectIntl } from 'react-intl'
 import { IChangeEvent } from 'react-jsonschema-form'
 
@@ -8,27 +8,35 @@ import {
   getExtension,
   getIframeImplementation,
 } from '../../../../utils/components'
+import EditorHeader from '../EditorHeader'
 
 import Form from './Form'
 import { getUiSchema } from './utils'
 
 interface CustomProps {
   editor: EditorContext
+  isLoading: boolean
   onChange: (event: IChangeEvent) => void
+  onClose: () => void
   onSave: () => void
   props: object
   runtime: RenderContext
+  shouldRenderSaveButton: boolean
 }
 
 type Props = CustomProps & ReactIntl.InjectedIntlProps
 
 const ComponentEditor: React.SFC<Props> = ({
+  children,
   editor,
   intl,
+  isLoading,
   onChange,
+  onClose,
   onSave,
   props,
   runtime,
+  shouldRenderSaveButton,
 }) => {
   const extension = getExtension(editor.editTreePath, runtime.extensions)
 
@@ -55,17 +63,29 @@ const ComponentEditor: React.SFC<Props> = ({
   }
 
   return (
-    <div className="bg-white flex flex-column justify-between size-editor w-100 pb3">
-      <Form
-        formContext={{ isLayoutMode: editor.mode === 'layout' }}
-        formData={props}
-        onChange={onChange}
-        onSubmit={onSave}
-        schema={schema as JSONSchema6}
-        uiSchema={getUiSchema(componentUiSchema, componentSchema)}
+    <Fragment>
+      <EditorHeader
+        isLoading={isLoading}
+        onClose={onClose}
+        onSave={onSave}
+        shouldRenderSaveButton={shouldRenderSaveButton}
+        title={componentSchema.title}
       />
-      <div id="form__error-list-template___alert" />
-    </div>
+      <div className="ph5 mt5">
+        {children}
+        <div className="bg-white flex flex-column justify-between size-editor w-100 pb3">
+          <Form
+            formContext={{ isLayoutMode: editor.mode === 'layout' }}
+            formData={props}
+            onChange={onChange}
+            onSubmit={onSave}
+            schema={schema as JSONSchema6}
+            uiSchema={getUiSchema(componentUiSchema, componentSchema)}
+          />
+          <div id="form__error-list-template___alert" />
+        </div>
+      </div>
+    </Fragment>
   )
 }
 
---
+++ react/components/EditorContainer/Sidebar/EditorHeader/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { IconArrowBack } from 'vtex.styleguide'
+
+import SaveButton from './SaveButton'
+
+interface Props {
+  isLoading?: boolean
+  onClose: () => void
+  onSave?: () => void
+  shouldRenderSaveButton?: boolean
+  title?: string
+}
+
+const EditorHeader: React.SFC<Props> = ({
+  isLoading,
+  onClose,
+  onSave,
+  shouldRenderSaveButton,
+  title,
+}) => (
+  <div className="w-100 flex items-center pl5 pt5 bt b--light-silver">
+    <span className="pointer" onClick={onClose}>
+      <IconArrowBack size={16} color="#585959" />
+    </span>
+    <div className="w-100 pl5 flex justify-between items-center">
+      {title && (
+        <h4 className="mv0 f6 fw5 dark-gray b--transparent ba bw1 pv3">
+          {title}
+        </h4>
+      )}
+      {shouldRenderSaveButton && onSave && (
+        <SaveButton
+          isLoading={isLoading || false}
+          onClick={onSave}
+          variation="tertiary"
+        />
+      )}
+    </div>
+  </div>
+)
+
+export default EditorHeader
