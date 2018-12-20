17f0b2e16624238572c9324aac7080dc9f3a7d7e
Alan Carpilovsky <alancarpilovsky@gmail.com>
2018-12-20 10:51:35 -0200

    Increase list child component name's left padding

--- react/components/ComponentsList/SortableList/SortableButton/Button.tsx
+++ react/components/ComponentsList/SortableList/SortableButton/Button.tsx
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl'
 
 interface Props {
   hasLeftPadding?: boolean
+  isChild?: boolean
   onEdit: (event: React.MouseEvent<HTMLButtonElement>) => void
   onMouseEnter: (event: React.MouseEvent<HTMLButtonElement>) => void
   onMouseLeave: (event: React.MouseEvent<HTMLButtonElement>) => void
@@ -12,28 +13,33 @@ interface Props {
 
 const Button = ({
   hasLeftPadding,
+  isChild,
   onEdit,
   onMouseEnter,
   onMouseLeave,
   title,
   treePath,
 }: Props) => (
-  <button
-    className={`w-100 pv5 bg-white hover-blue dark-gray pointer tl bn ${
-      hasLeftPadding ? 'pl7 pr0' : 'ph0'
-    }`}
-    data-tree-path={treePath}
-    key={treePath}
-    onClick={onEdit}
-    onMouseEnter={onMouseEnter}
-    onMouseLeave={onMouseLeave}
-    style={{ animationDuration: '0.2s' }}
-    type="button"
-  >
-    <FormattedMessage id={title}>
-      {text => <span className="pl2 f6 fw5 track-1">{text}</span>}
-    </FormattedMessage>
-  </button>
-)
+    <button
+      className={`w-100 pv5 bg-white hover-blue dark-gray pointer tl bn ${
+        hasLeftPadding ? 'pl7 pr0' : 'ph0'
+        }`}
+      data-tree-path={treePath}
+      key={treePath}
+      onClick={onEdit}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      style={{ animationDuration: '0.2s' }}
+      type="button"
+    >
+      <FormattedMessage id={title}>
+        {text => (
+          <span className={`f6 fw5 track-1 ${isChild ? 'pl7' : 'pl2'}`}>
+            {text}
+          </span>
+        )}
+      </FormattedMessage>
+    </button>
+  )
 
 export default Button
