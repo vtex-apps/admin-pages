8b2f4b1dfac31fed4fcb8fe5463037aeb6ed8e05
Alan Carpilovsky <alancarpilovsky@gmail.com>
2018-12-20 16:11:39 -0200

    Fix arrow icon SVG attribute name

--- react/components/icons/ArrowIcon.tsx
+++ react/components/icons/ArrowIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ArrowIcon: React.SFC = () => (
   <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
-    <g clip-path="url(#clip0)">
+    <g clipPath="url(#clip0)">
       <rect width="16" height="16" fill="none" />
       <path d="M8 10.5L11.5 7L4.5 7L8 10.5Z" fill="#727273" />
     </g>
