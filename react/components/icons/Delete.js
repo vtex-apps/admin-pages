ef6f2f76bf7f25cdd8dc780ca35a22a0fbc3174e
g-lefundes <gabriel.lefundes@vtex.com.br>
2018-12-26 19:03:02 -0200

    convert some tags from dash-case to camelCase

--- react/components/icons/Delete.js
+++ react/components/icons/Delete.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default class Delete extends React.Component {
 
   render() {
-    return ( 
+    return (
       <svg width="16" height="16" viewBox="0 0 16 16" fill="none">
         <circle cx="8" cy="8" r="8" fill="#CACBCC"/>
-        <path d="M11 5L5 11" stroke="white" stroke-width="1.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M5 5L11 11" stroke="white" stroke-width="1.6" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M11 5L5 11" stroke="white" strokeWidth="1.6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
+        <path d="M5 5L11 11" stroke="white" strokeWidth="1.6" strokeMiterlimit="10" strokeLinecap="round" strokeLinejoin="round"/>
       </svg>
     )
   }
