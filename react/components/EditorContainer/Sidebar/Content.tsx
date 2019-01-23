f2f209ee2656f774a127794c2d4dc1a5671ec541
Alan Carpilovsky <alancarpilovsky@gmail.com>
2019-01-23 15:34:58 -0200

    Refactor and fix some component list functions

--- react/components/EditorContainer/Sidebar/Content.tsx
+++ react/components/EditorContainer/Sidebar/Content.tsx
@@ -1,163 +1,77 @@
-import React, { Component } from 'react'
-import { canUseDOM } from 'vtex.render-runtime'
+import React from 'react'
 import { Spinner } from 'vtex.styleguide'
 
-import { getIframeImplementation } from '../../../utils/components'
+import { getIframeRenderComponents } from '../../../utils/components'
 
 import ComponentSelector from './ComponentSelector'
 import ConfigurationList from './ConfigurationList'
 import { FormMetaConsumer } from './FormMetaContext'
 import { ModalConsumer } from './ModalContext'
 import TemplateEditor from './TemplateEditor'
-import { SidebarComponent } from './typings'
+import { getComponents } from './utils'
 
 interface Props {
   editor: EditorContext
   highlightHandler: (treePath: string | null) => void
-  runtime: RenderContext
+  iframeRuntime: RenderContext
 }
 
-class Content extends Component<Props> {
-  public render() {
-    const { editor, highlightHandler, runtime } = this.props
-
-    if (!runtime) {
-      return (
-        <div className="mt5 flex justify-center">
-          <Spinner />
-        </div>
-      )
-    }
-
-    if (editor.editTreePath === null) {
-      return (
-        <ComponentSelector
-          components={this.getComponents(runtime)}
-          editor={editor}
-          highlightHandler={highlightHandler}
-          iframeRuntime={runtime}
-        />
-      )
-    }
-
+const Content: React.SFC<Props> = ({
+  editor,
+  highlightHandler,
+  iframeRuntime,
+}) => {
+  if (!iframeRuntime) {
     return (
-      <FormMetaConsumer>
-        {formMeta => (
-          <ModalConsumer>
-            {modal =>
-              editor.mode === 'layout' ? (
-                <TemplateEditor
-                  editor={editor}
-                  formMeta={formMeta}
-                  modal={modal}
-                  runtime={runtime}
-                />
-              ) : (
-                <ConfigurationList
-                  editor={editor}
-                  formMeta={formMeta}
-                  modal={modal}
-                  runtime={runtime}
-                />
-              )
-            }
-          </ModalConsumer>
-        )}
-      </FormMetaConsumer>
+      <div className="mt5 flex justify-center">
+        <Spinner />
+      </div>
     )
   }
 
-  private getComponentElement = (treePath: string) => {
-    const {
-      editor: { iframeWindow },
-    } = this.props
-
-    if (!canUseDOM) {
-      return undefined
-    }
-
-    return iframeWindow.document.querySelector(
-      `[data-extension-point="${treePath}"]`,
-    )
-  }
-
-  private getComponentSchema(treePath: string) {
-    const {
-      runtime: { extensions },
-    } = this.props
-
-    const { component, props = {} } = extensions[treePath]
-
-    const ComponentImpl =
-      (component && getIframeImplementation(component)) || undefined
-
-    return (
-      ComponentImpl &&
-      ((ComponentImpl.hasOwnProperty('schema') && ComponentImpl.schema) ||
-        (ComponentImpl.getSchema && ComponentImpl.getSchema(props)))
-    )
-  }
-
-  private getComponents(runtime: RenderContext) {
-    return Object.keys(runtime.extensions)
-      .filter(this.isValidExtension)
-      .sort(this.sortComponents)
-      .map<SidebarComponent>(treePath => ({
-        name: this.getComponentSchema(treePath)!.title!,
-        treePath,
-      }))
-  }
-
-  private isValidExtension = (treePath: string) => {
-    const {
-      runtime: { pages, page },
-    } = this.props
-
-    const schema = this.getComponentSchema(treePath)
-
-    return (
-      schema &&
-      schema.title &&
-      !isDifferentPage(treePath, page, Object.keys(pages))
-    )
-  }
-
-  private sortComponents = (treePathA: string, treePathB: string) => {
-    const componentA = this.getComponentElement(treePathA)
-
-    if (!componentA) {
-      return 1
-    }
-
-    const componentB = this.getComponentElement(treePathB)
-
-    if (!componentB) {
-      return -1
-    }
-
-    const { x: xA, y: yA } = componentA.getBoundingClientRect() as DOMRect
-    const { x: xB, y: yB } = componentB.getBoundingClientRect() as DOMRect
-
-    if (yA > yB || (yA === yB && xA > xB)) {
-      return 1
-    }
-
-    return -1
-  }
-}
-
-const isDifferentPage = (treePath: string, page: string, pages: string[]) => {
-  if (treePath.startsWith(page)) {
-    return false
-  }
-
-  const currentPageLevel = page.split('/').length
-
-  const sameLevelPages = pages.filter(
-    (p: string) => p.split('/').length === currentPageLevel,
+  const components = getComponents(
+    iframeRuntime.extensions,
+    getIframeRenderComponents(),
+    iframeRuntime.page,
+    Object.keys(iframeRuntime.pages)
   )
 
-  return !!sameLevelPages.find((p: string) => treePath.startsWith(p))
+  if (editor.editTreePath === null) {
+    return (
+      <ComponentSelector
+        components={components}
+        editor={editor}
+        highlightHandler={highlightHandler}
+        iframeRuntime={iframeRuntime}
+      />
+    )
+  }
+
+  return (
+    <FormMetaConsumer>
+      {formMeta => (
+        <ModalConsumer>
+          {modal =>
+            editor.mode === 'layout' ? (
+              <TemplateEditor
+                editor={editor}
+                formMeta={formMeta}
+                modal={modal}
+                iframeRuntime={iframeRuntime}
+              />
+            ) : (
+              <ConfigurationList
+                editor={editor}
+                formMeta={formMeta}
+                modal={modal}
+                iframeRuntime={iframeRuntime}
+              />
+            )
+          }
+        </ModalConsumer>
+      )}
+    </FormMetaConsumer>
+  )
 }
 
 export default Content
