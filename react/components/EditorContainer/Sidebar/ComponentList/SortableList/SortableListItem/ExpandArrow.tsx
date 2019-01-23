b3f722b950514ddb4109841abaed572a8c7e47a0
Alan Carpilovsky <alancarpilovsky@gmail.com>
2019-01-23 15:59:03 -0200

    Change expand arrow icon and rotation logic

--- react/components/EditorContainer/Sidebar/ComponentList/SortableList/SortableListItem/ExpandArrow.tsx
+++ react/components/EditorContainer/Sidebar/ComponentList/SortableList/SortableListItem/ExpandArrow.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const ExpandArrow: React.SFC<Props> = ({ isExpanded, onClick }) => (
   <SideItem isPointer onClick={onClick}>
-    <div className={`flex items-center ${isExpanded ? 'rotate-180' : ''}`}>
+    <div className={`flex items-center ${isExpanded ? '' : 'rotate-90'}`}>
       <ArrowIcon />
     </div>
   </SideItem>
