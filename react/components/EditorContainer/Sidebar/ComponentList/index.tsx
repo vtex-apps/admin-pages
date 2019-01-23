c6ad0f62d5323c5d3ce07f0492e98272a4dd52b6
Alan Carpilovsky <alancarpilovsky@gmail.com>
2019-01-23 16:52:28 -0200

    Save extension on sortable list to persist order

--- react/components/EditorContainer/Sidebar/ComponentList/index.tsx
+++ react/components/EditorContainer/Sidebar/ComponentList/index.tsx
@@ -1,26 +1,33 @@
-import { equals, findIndex, last } from 'ramda'
+import { equals, findIndex, last, path } from 'ramda'
 import React, { Component, Fragment } from 'react'
+import { compose, graphql, MutationFn } from 'react-apollo'
+import { InjectedIntlProps, injectIntl } from 'react-intl'
 import { arrayMove, SortEndHandler } from 'react-sortable-hoc'
-import { Button, ToastConsumer } from 'vtex.styleguide'
+import { Button, ToastConsumerRenderProps } from 'vtex.styleguide'
 
+import SaveExtension from '../../../queries/SaveExtension.graphql'
 import { SidebarComponent } from '../typings'
 
 import SortableList from './SortableList'
 import { NormalizedComponent } from './typings'
 import { getParentTreePath, normalizeComponents } from './utils'
 
-interface Props {
+interface CustomProps {
   components: SidebarComponent[]
   editor: EditorContext
   highlightHandler: (treePath: string | null) => void
   iframeRuntime: RenderContextProps['runtime']
   onMouseEnterComponent: (event: React.MouseEvent<HTMLDivElement>) => void
   onMouseLeaveComponent: () => void
+  saveExtension: MutationFn
 }
+
+type Props = CustomProps & InjectedIntlProps & ToastConsumerRenderProps
 
 interface State {
   components: NormalizedComponent[]
   initialComponents: SidebarComponent[]
+  isLoadingMutation: boolean
 }
 
 class ComponentList extends Component<Props, State> {
@@ -39,11 +46,18 @@ class ComponentList extends Component<Props, State> {
     this.state = {
       components: normalizeComponents(props.components),
       initialComponents: props.components,
+      isLoadingMutation: false,
     }
   }
 
   public render() {
-    const { editor, onMouseEnterComponent, onMouseLeaveComponent } = this.props
+    const {
+      editor,
+      intl,
+      onMouseEnterComponent,
+      onMouseLeaveComponent,
+      showToast,
+    } = this.props
 
     const isSortable = editor.mode === 'layout'
 
@@ -51,27 +65,32 @@ class ComponentList extends Component<Props, State> {
       <Fragment>
         <div className="bb bw1 b--light-silver" />
         <div className="flex flex-column justify-between flex-grow-1">
-          <ToastConsumer>
-            {({ showToast }) => (
-              <SortableList
-                components={this.state.components}
-                isSortable={isSortable}
-                lockAxis="y"
-                onEdit={this.handleEdit}
-                onMouseEnter={onMouseEnterComponent}
-                onMouseLeave={onMouseLeaveComponent}
-                onSortEnd={this.handleSortEnd}
-                showToast={showToast}
-                useDragHandle={isSortable}
-              />
-            )}
-          </ToastConsumer>
+          <SortableList
+            components={this.state.components}
+            isSortable={isSortable}
+            lockAxis="y"
+            onEdit={this.handleEdit}
+            onMouseEnter={onMouseEnterComponent}
+            onMouseLeave={onMouseLeaveComponent}
+            onSortEnd={this.handleSortEnd}
+            showToast={showToast}
+            useDragHandle={isSortable}
+          />
+          )}
           <div className="bt b--light-silver" />
           <div className="bt bw1 b--light-silver" style={{ marginTop: 'auto' }}>
             <Button disabled variation="tertiary">
               undo (i18n)
             </Button>
-            <Button variation="tertiary">save (i18n)</Button>
+            <Button
+              isLoading={this.state.isLoadingMutation}
+              variation="tertiary"
+              onClick={this.handleSaveReorder}
+            >
+              {intl.formatMessage({
+                id: 'pages.editor.component-list.save.button',
+              })}
+            </Button>
           </div>
         </div>
       </Fragment>
@@ -86,6 +105,59 @@ class ComponentList extends Component<Props, State> {
     editor.editExtensionPoint(treePath as string)
 
     highlightHandler(null)
+  }
+
+  private handleSaveReorder = async () => {
+    const { iframeRuntime, intl, saveExtension } = this.props
+
+    const iframeWindow = (document.getElementById(
+      'store-iframe'
+    ) as HTMLIFrameElement).contentWindow
+
+    const iframeCurrentPage = iframeRuntime.page
+    const extension = iframeRuntime.extensions[iframeCurrentPage]
+    const configurationId = path(['configurationIds', 0])(extension)
+
+    if (iframeWindow === null) {
+      throw new Error('iframeWindow is null')
+    }
+
+    try {
+      this.setState({
+        isLoadingMutation: true,
+      })
+
+      await saveExtension({
+        variables: {
+          allMatches: true,
+          conditions: [],
+          configurationId,
+          device: 'any',
+          extensionName: iframeCurrentPage,
+          label: null,
+          path: iframeWindow.location.pathname,
+          propsJSON: JSON.stringify(extension.props),
+          routeId: 'store',
+          scope: 'route',
+        },
+      })
+
+      this.setState({
+        isLoadingMutation: false,
+      })
+
+      this.props.showToast(
+        intl.formatMessage({ id: 'pages.editor.component-list.save.success' })
+      )
+    } catch (e) {
+      this.props.showToast(
+        intl.formatMessage({ id: 'pages.editor.component-list.save.error' })
+      )
+    } finally {
+      this.setState({
+        isLoadingMutation: false,
+      })
+    }
   }
 
   private handleSortEnd: SortEndHandler = ({ oldIndex, newIndex }) => {
@@ -141,4 +213,7 @@ class ComponentList extends Component<Props, State> {
   }
 }
 
-export default ComponentList
+export default compose(
+  injectIntl,
+  graphql(SaveExtension, { name: 'saveExtension' })
+)(ComponentList)
