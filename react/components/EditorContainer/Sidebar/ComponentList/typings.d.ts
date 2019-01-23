d1d9a0360b4bc71854c1ee86b9c9642cbcb5cf1e
Alan Carpilovsky <alancarpilovsky@gmail.com>
2019-01-23 17:40:15 -0200

    Reorder undo and buttons disabled based on changes

--- react/components/EditorContainer/Sidebar/ComponentList/typings.d.ts
+++ react/components/EditorContainer/Sidebar/ComponentList/typings.d.ts
@@ -4,3 +4,8 @@ export interface NormalizedComponent extends SidebarComponent {
   components?: NormalizedComponent[]
   isSortable: boolean
 }
+
+export interface ReorderChange {
+  target: string
+  order: string[]
+}
