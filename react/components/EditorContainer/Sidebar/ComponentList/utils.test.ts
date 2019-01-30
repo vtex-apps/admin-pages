c7e6f9d42d7992fe9f8b374aa7a927f0bef4569c
Alan Carpilovsky <alancarpilovsky@gmail.com>
2019-01-30 17:57:02 -0200

    Adapt component reorder utils to blocks

--- react/components/EditorContainer/Sidebar/ComponentList/utils.test.ts
+++ react/components/EditorContainer/Sidebar/ComponentList/utils.test.ts
@@ -1,55 +1,43 @@
 import { SidebarComponent } from '../typings'
 
 import { NormalizedComponent } from './typings'
-import {
-  getParentTreePath,
-  isRootComponent,
-  normalizeComponents
-} from './utils'
+import { getParentTreePath, isRootComponent, normalize } from './utils'
 
-describe('normalizeComponents', () => {
+describe('getParentTreePath', () => {
+  it('returns parent treePath', () => {
+    expect(getParentTreePath('store.home/shelf#home')).toBe('store.home')
+  })
+
+  it('handles strings with no delimiter', () => {
+    expect(getParentTreePath('store')).toBe('store')
+  })
+})
+
+describe('normalize', () => {
   it('handles empty array inputs', () => {
     const input: SidebarComponent[] = []
 
     const expectedOutput: NormalizedComponent[] = []
 
-    expect(normalizeComponents(input)).toEqual(expectedOutput)
+    expect(normalize(input)).toEqual(expectedOutput)
   })
 
   it('nests store.home/ level components', () => {
     const input: SidebarComponent[] = [
-      {
-        name: 'editor.menu',
-        treePath: 'store.home/header.full/menu-link'
-      },
-      {
-        name: 'editor.header.title',
-        treePath: 'store.home/header.full'
-      },
-      {
-        name: 'editor.minicart.title',
-        treePath: 'store.home/header.full/minicart'
-      },
-      {
-        name: 'editor.login.title',
-        treePath: 'store.home/header.full/login'
-      },
+      { name: 'editor.footer.title', treePath: 'store.home/$after_0' },
+      { name: 'editor.header.title', treePath: 'store.home/$before_0' },
       {
         name: 'editor.category-menu.title',
-        treePath: 'store.home/header.full/category-menu'
+        treePath: 'store.home/$before_0/category-menu'
       },
+      { name: 'editor.login.title', treePath: 'store.home/$before_0/login' },
+      { name: 'editor.menu', treePath: 'store.home/$before_0/menu-link' },
       {
-        name: 'editor.carousel.title',
-        treePath: 'store.home/carousel'
+        name: 'editor.minicart.title',
+        treePath: 'store.home/$before_0/minicart'
       },
-      {
-        name: 'editor.shelf.title',
-        treePath: 'store.home/shelf'
-      },
-      {
-        name: 'editor.footer.title',
-        treePath: 'store.home/footer'
-      }
+      { name: 'editor.carousel.title', treePath: 'store.home/carousel#home' },
+      { name: 'editor.shelf.title', treePath: 'store.home/shelf#home' }
     ]
 
     const expectedOutput: NormalizedComponent[] = [
@@ -57,227 +45,245 @@ describe('normalizeComponents', () => {
         components: [
           {
             isSortable: false,
-            name: 'editor.menu',
-            treePath: 'store.home/header.full/menu-link'
-          },
-          {
-            isSortable: false,
-            name: 'editor.minicart.title',
-            treePath: 'store.home/header.full/minicart'
+            name: 'editor.category-menu.title',
+            treePath: 'store.home/$before_0/category-menu'
           },
           {
             isSortable: false,
             name: 'editor.login.title',
-            treePath: 'store.home/header.full/login'
+            treePath: 'store.home/$before_0/login'
           },
           {
             isSortable: false,
-            name: 'editor.category-menu.title',
-            treePath: 'store.home/header.full/category-menu'
-          }
-        ],
-        isSortable: true,
-        name: 'editor.header.title',
-        treePath: 'store.home/header.full'
-      },
-      {
-        isSortable: true,
-        name: 'editor.carousel.title',
-        treePath: 'store.home/carousel'
-      },
-      {
-        isSortable: true,
-        name: 'editor.shelf.title',
-        treePath: 'store.home/shelf'
-      },
-      {
-        isSortable: true,
-        name: 'editor.footer.title',
-        treePath: 'store.home/footer'
-      }
-    ]
-
-    expect(normalizeComponents(input)).toEqual(expectedOutput)
-  })
-
-  it('nests regular components', () => {
-    const input: SidebarComponent[] = [
-      {
-        name: 'editor.header.title',
-        treePath: 'store.home/header.full'
-      },
-      {
-        name: 'editor.carousel.title',
-        treePath: 'store.home/carousel'
-      },
-      {
-        name: 'editor.shelf.title',
-        treePath: 'store.home/shelf'
-      },
-      {
-        name: 'editor.product-summary.title',
-        treePath: 'store.home/shelf/product-summary'
-      },
-      {
-        name: 'editor.footer.title',
-        treePath: 'store.home/footer'
-      }
-    ]
-
-    const expectedOutput: NormalizedComponent[] = [
-      {
-        isSortable: true,
-        name: 'editor.header.title',
-        treePath: 'store.home/header.full'
-      },
-      {
-        isSortable: true,
-        name: 'editor.carousel.title',
-        treePath: 'store.home/carousel'
-      },
-      {
-        components: [
+            name: 'editor.menu',
+            treePath: 'store.home/$before_0/menu-link'
+          },
           {
             isSortable: false,
-            name: 'editor.product-summary.title',
-            treePath: 'store.home/shelf/product-summary'
+            name: 'editor.minicart.title',
+            treePath: 'store.home/$before_0/minicart'
           }
         ],
-        isSortable: true,
-        name: 'editor.shelf.title',
-        treePath: 'store.home/shelf'
+        isSortable: false,
+        name: 'editor.header.title',
+        treePath: 'store.home/$before_0'
       },
       {
         isSortable: true,
+        name: 'editor.carousel.title',
+        treePath: 'store.home/carousel#home'
+      },
+      {
+        isSortable: true,
+        name: 'editor.shelf.title',
+        treePath: 'store.home/shelf#home'
+      },
+      {
+        isSortable: false,
         name: 'editor.footer.title',
-        treePath: 'store.home/footer'
+        treePath: 'store.home/$after_0'
       }
     ]
 
-    expect(normalizeComponents(input)).toEqual(expectedOutput)
+    expect(normalize(input)).toEqual(expectedOutput)
   })
 
-  it('handles multilevel nesting', () => {
+  it('flattens multilevel components', () => {
     const input: SidebarComponent[] = [
       {
         name: 'editor.header.title',
-        treePath: 'store.home/header.full'
+        treePath: 'store.home/$before_0'
       },
       {
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel'
+        treePath: 'store.home/carousel#home'
       },
       {
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf'
+        treePath: 'store.home/shelf#home'
       },
       {
         name: 'editor.product-summary.title',
-        treePath: 'store.home/shelf/product-summary'
+        treePath: 'store.home/shelf#home/product-summary'
       },
       {
         name: 'editor.product-rating.title',
-        treePath: 'store.home/shelf/product-summary/product-rating'
+        treePath: 'store.home/shelf#home/product-summary/product-rating'
       },
       {
         name: 'editor.footer.title',
-        treePath: 'store.home/footer'
+        treePath: 'store.home/$after_0'
       }
     ]
 
     const expectedOutput: NormalizedComponent[] = [
       {
-        isSortable: true,
+        isSortable: false,
         name: 'editor.header.title',
-        treePath: 'store.home/header.full'
+        treePath: 'store.home/$before_0'
       },
       {
         isSortable: true,
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel'
+        treePath: 'store.home/carousel#home'
       },
       {
         components: [
           {
             isSortable: false,
             name: 'editor.product-summary.title',
-            treePath: 'store.home/shelf/product-summary'
+            treePath: 'store.home/shelf#home/product-summary'
           },
           {
             isSortable: false,
             name: 'editor.product-rating.title',
-            treePath: 'store.home/shelf/product-summary/product-rating'
+            treePath: 'store.home/shelf#home/product-summary/product-rating'
           }
         ],
         isSortable: true,
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf'
+        treePath: 'store.home/shelf#home'
       },
       {
-        isSortable: true,
+        isSortable: false,
         name: 'editor.footer.title',
-        treePath: 'store.home/footer'
+        treePath: 'store.home/$after_0'
       }
     ]
 
-    expect(normalizeComponents(input)).toEqual(expectedOutput)
+    expect(normalize(input)).toEqual(expectedOutput)
   })
 
-  it('handles unordered nestable components', () => {
+  it('handles unordered components', () => {
     const input: SidebarComponent[] = [
       {
-        name: 'editor.header.title',
-        treePath: 'store.home/header.full'
-      },
-      {
-        name: 'editor.carousel.title',
-        treePath: 'store.home/carousel'
+        name: 'editor.footer.title',
+        treePath: 'store.home/$after_0'
       },
       {
         name: 'editor.product-summary.title',
-        treePath: 'store.home/shelf/product-summary'
+        treePath: 'store.home/shelf#home/product-summary'
+      },
+      {
+        name: 'editor.header.title',
+        treePath: 'store.home/$before_0'
+      },
+      {
+        name: 'editor.carousel.title',
+        treePath: 'store.home/carousel#home'
       },
       {
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf'
-      },
-      {
-        name: 'editor.footer.title',
-        treePath: 'store.home/footer'
+        treePath: 'store.home/shelf#home'
       }
     ]
 
     const expectedOutput: NormalizedComponent[] = [
       {
-        isSortable: true,
+        isSortable: false,
         name: 'editor.header.title',
-        treePath: 'store.home/header.full'
+        treePath: 'store.home/$before_0'
       },
       {
         isSortable: true,
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel'
+        treePath: 'store.home/carousel#home'
       },
       {
         components: [
           {
             isSortable: false,
             name: 'editor.product-summary.title',
-            treePath: 'store.home/shelf/product-summary'
+            treePath: 'store.home/shelf#home/product-summary'
           }
         ],
         isSortable: true,
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf'
+        treePath: 'store.home/shelf#home'
       },
       {
-        isSortable: true,
+        isSortable: false,
         name: 'editor.footer.title',
-        treePath: 'store.home/footer'
+        treePath: 'store.home/$after_0'
       }
     ]
 
-    expect(normalizeComponents(input)).toEqual(expectedOutput)
+    expect(normalize(input)).toEqual(expectedOutput)
+  })
+
+  it(`handles components with 'around' blocks`, () => {
+    const input: SidebarComponent[] = [
+      {
+        name: 'editor.header.title',
+        treePath: 'store.home/$before_0'
+      },
+      {
+        name: 'editor.carousel.title',
+        treePath: 'store.home/carousel#home'
+      },
+      {
+        name: 'editor.product-summary.title',
+        treePath: 'store.home/shelf#home/product-summary'
+      },
+      {
+        name: 'editor.around-shelf.title',
+        treePath: 'store.home/shelf#home/$around_0'
+      },
+      {
+        name: 'editor.shelf.title',
+        treePath: 'store.home/shelf#home'
+      },
+      {
+        name: 'editor.around-carousel.title',
+        treePath: 'store.home/carousel#home/$around_0'
+      },
+      {
+        name: 'editor.footer.title',
+        treePath: 'store.home/$after_0'
+      }
+    ]
+
+    const expectedOutput: NormalizedComponent[] = [
+      {
+        isSortable: false,
+        name: 'editor.header.title',
+        treePath: 'store.home/$before_0'
+      },
+      {
+        isSortable: false,
+        name: 'editor.around-carousel.title',
+        treePath: 'store.home/carousel#home/$around_0'
+      },
+      {
+        isSortable: true,
+        name: 'editor.carousel.title',
+        treePath: 'store.home/carousel#home'
+      },
+      {
+        isSortable: false,
+        name: 'editor.around-shelf.title',
+        treePath: 'store.home/shelf#home/$around_0'
+      },
+      {
+        components: [
+          {
+            isSortable: false,
+            name: 'editor.product-summary.title',
+            treePath: 'store.home/shelf#home/product-summary'
+          }
+        ],
+        isSortable: true,
+        name: 'editor.shelf.title',
+        treePath: 'store.home/shelf#home'
+      },
+      {
+        isSortable: false,
+        name: 'editor.footer.title',
+        treePath: 'store.home/$after_0'
+      }
+    ]
+
+    expect(normalize(input)).toEqual(expectedOutput)
   })
 })
 
