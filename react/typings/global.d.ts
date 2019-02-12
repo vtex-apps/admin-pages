88a928c4731ef935a6e14878b22a3216861e1881
Alan Carpilovsky <alancarpilovsky@gmail.com>
2019-02-12 15:20:11 -0200

    Make component reordering work

--- react/typings/global.d.ts
+++ react/typings/global.d.ts
@@ -21,7 +21,7 @@ declare global {
     around?: string[]
     before?: string[]
     blockId?: string
-    blocks?: string[]
+    blocks?: InnerBlock[]
     component: string | null
     configurationsIds?: string[]
     props?: any
@@ -244,4 +244,9 @@ declare global {
   }
 
   type BlockPath = FormattedBlock[]
+}
+
+interface InnerBlock {
+  blockId: string
+  extensionPointId: string
 }
