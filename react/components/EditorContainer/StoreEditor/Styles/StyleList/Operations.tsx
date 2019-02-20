874594325a48c5035e383c2dc811ceca31a05e96
Pedro Sousa <pedroteosousa@gmail.com>
2019-02-20 17:06:49 -0300

    Use Mutation and Query components on StyleList

---
+++ react/components/EditorContainer/StoreEditor/Styles/StyleList/Operations.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Mutation, MutationFn, Query, QueryResult } from 'react-apollo'
+
+import CreateStyle from './queries/CreateStyle.graphql'
+import DeleteStyle from './queries/DeleteStyle.graphql'
+import ListStyles from './queries/ListStyles.graphql'
+import SaveSelectedStyle from './queries/SaveSelectedStyle.graphql'
+
+interface Props {
+  children: (mutations: OperationsObj) => React.ReactNode
+}
+
+interface ListStyles {
+  listStyles: Style[]
+}
+
+interface OperationsObj {
+  listStyles: QueryResult<ListStyles>
+  saveSelectedStyle: MutationFn
+  createStyle: MutationFn
+  deleteStyle: MutationFn
+}
+
+const Operations = (props: Props) => (
+  <Query query={ListStyles} fetchPolicy={'no-cache'}>
+    {listStyles => (
+      <Mutation
+        mutation={SaveSelectedStyle}
+        refetchQueries={() => [{ query: ListStyles }]}
+      >
+        {saveSelectedStyle => (
+          <Mutation
+            mutation={CreateStyle}
+            refetchQueries={() => [{ query: ListStyles }]}
+          >
+            {createStyle => (
+              <Mutation
+                mutation={DeleteStyle}
+                refetchQueries={() => [{ query: ListStyles }]}
+              >
+                {deleteStyle =>
+                  props.children({
+                    createStyle,
+                    deleteStyle,
+                    listStyles,
+                    saveSelectedStyle,
+                  })
+                }
+              </Mutation>
+            )}
+          </Mutation>
+        )}
+      </Mutation>
+    )}
+  </Query>
+)
+
+export default Operations
