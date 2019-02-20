9c24a98dfcfb7a0196d236216b319b98b56c1e51
Pedro Sousa <pedroteosousa@gmail.com>
2019-02-20 17:06:49 -0300

    Use Mutation and Query components on StyleList

--- react/components/EditorContainer/StoreEditor/Styles/StyleList/index.tsx
+++ react/components/EditorContainer/StoreEditor/Styles/StyleList/index.tsx
@@ -1,147 +1,83 @@
 import { find } from 'ramda'
-import React, { Component } from 'react'
-import { compose, graphql, withApollo, WithApolloClient } from 'react-apollo'
+import React from 'react'
 import { ButtonWithIcon, Spinner } from 'vtex.styleguide'
 
-import CreateStyle from './queries/CreateStyle.graphql'
-import DeleteStyle from './queries/DeleteStyle.graphql'
-import ListStyles from './queries/ListStyles.graphql'
-import SaveSelectedStyle from './queries/SaveSelectedStyle.graphql'
+import Operations from './Operations'
 
 import CreateNewIcon from './icons/CreateNewIcon'
 import StyleCard from './StyleCard'
 
-interface ListStylesQuery {
-  listStyles: Style[]
-  loading: boolean
-  refetch: (variables?: object) => void
-}
-
-interface CustomProps {
+interface Props {
   startEditing: (style: Style) => void
   iframeWindow: Window
-  listStylesQuery: ListStylesQuery
 }
 
-type Props = WithApolloClient<CustomProps>
+const StyleList: React.SFC<Props> = ({ startEditing, iframeWindow }) => {
+  return (
+    <Operations>
+      {({
+        listStyles: { data, loading },
+        saveSelectedStyle,
+        createStyle,
+        deleteStyle,
+      }) => {
+        const listStyles = data && data.listStyles
+        console.log({ listStyles })
 
-class StyleList extends Component<Props, {}> {
-  public render() {
-    const {
-      listStylesQuery: { listStyles, loading },
-      startEditing,
-    } = this.props
+        const selected = listStyles && find(style => style.selected, listStyles)
+        if (selected) {
+          updateStyleTag(selected.path, iframeWindow)
+        }
 
-    const selected = listStyles && find(style => style.selected, listStyles)
-    if (selected) {
-      this.updateStyleTag(selected.path)
-    }
+        return loading ? (
+          <div className="pt7 flex justify-around">
+            <Spinner />
+          </div>
+        ) : (
+          <div className="flex flex-column ph3 h-100">
+            <div className="flex justify-between mv5 ml5 items-center">
+              <span className="f3">Styles</span>
+              <ButtonWithIcon
+                icon={<CreateNewIcon />}
+                variation="tertiary"
+                onClick={() => createStyle({ variables: { name: 'Untitled' } })}
+              >
+                New
+              </ButtonWithIcon>
+            </div>
+            <div className="flex flex-column flex-grow-1 overflow-scroll">
+              {listStyles &&
+                listStyles.map(style => (
+                  <StyleCard
+                    key={style.id}
+                    style={style}
+                    selectStyle={({ id }: Style) =>
+                      saveSelectedStyle({ variables: { id } })
+                    }
+                    deleteStyle={({ id }: Style) =>
+                      deleteStyle({ variables: { id } })
+                    }
+                    duplicateStyle={({ name, config }: Style) =>
+                      createStyle({
+                        variables: { name: `Copy of ${name}`, config },
+                      })
+                    }
+                    startEditing={startEditing}
+                  />
+                ))}
+            </div>
+          </div>
+        )
+      }}
+    </Operations>
+  )
+}
 
-    return loading ? (
-      <div className="pt7 flex justify-around">
-        <Spinner />
-      </div>
-    ) : (
-      <div className="flex flex-column ph3 h-100">
-        <div className="flex justify-between mv5 ml5 items-center">
-          <span className="f3">Styles</span>
-          <ButtonWithIcon
-            icon={<CreateNewIcon />}
-            variation="tertiary"
-            onClick={() => this.createStyle()}
-          >
-            New
-          </ButtonWithIcon>
-        </div>
-        <div className="flex flex-column flex-grow-1 overflow-scroll">
-          {listStyles.map(style => (
-            <StyleCard
-              key={style.id}
-              style={style}
-              selectStyle={this.selectStyle}
-              deleteStyle={this.deleteStyle}
-              duplicateStyle={this.duplicateStyle}
-              startEditing={startEditing}
-            />
-          ))}
-        </div>
-      </div>
-    )
-  }
-
-  private selectStyle = async (style: Style) => {
-    const {
-      client,
-      listStylesQuery: { refetch },
-    } = this.props
-
-    try {
-      await client.mutate<{ saveSelectedStyle: BasicStyle }>({
-        mutation: SaveSelectedStyle,
-        variables: {
-          id: style.id,
-        },
-      })
-      refetch()
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  private deleteStyle = async (style: Style) => {
-    // TODO: open a modal before deleting
-    const {
-      client,
-      listStylesQuery: { refetch },
-    } = this.props
-
-    try {
-      await client.mutate<{ deleteStyle: BasicStyle }>({
-        mutation: DeleteStyle,
-        variables: {
-          id: style.id,
-        },
-      })
-      refetch()
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  private duplicateStyle = async (style: Style) => {
-    await this.createStyle(`Copy of ${style.name}`, style.config)
-  }
-
-  private createStyle = async (name?: string, config?: TachyonsConfig) => {
-    const {
-      client,
-      listStylesQuery: { refetch },
-    } = this.props
-
-    try {
-      await client.mutate<{ createStyle: BasicStyle }>({
-        mutation: CreateStyle,
-        variables: {
-          config: config || {},
-          name: name || 'Untitled',
-        },
-      })
-      refetch()
-    } catch (err) {
-      console.error(err)
-    }
-  }
-
-  private updateStyleTag(path: string) {
-    const { iframeWindow } = this.props
-    const styleLinkElement = iframeWindow.document.getElementById('style_link')
-    if (styleLinkElement) {
-      styleLinkElement.setAttribute('href', path)
-    }
+const updateStyleTag = (path: string, iframeWindow: Window) => {
+  const styleLinkElement = iframeWindow.document.getElementById('style_link')
+  if (styleLinkElement) {
+    styleLinkElement.setAttribute('href', path)
   }
 }
 
-export default compose(
-  graphql(ListStyles, { name: 'listStylesQuery' }),
-  withApollo
-)(StyleList)
+export default StyleList
