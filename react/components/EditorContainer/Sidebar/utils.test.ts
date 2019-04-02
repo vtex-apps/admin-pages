1b1c4307213f401f32cedce829bc9ef478514cac
Thiago Murakami <thiago.murakami@gmail.com>
2019-04-02 17:29:59 -0300

    Define template and treePath on Content and pass it down to other components. Move isSitewide logic to utils and create tests.

--- react/components/EditorContainer/Sidebar/utils.test.ts
+++ react/components/EditorContainer/Sidebar/utils.test.ts
@@ -1,57 +1,57 @@
-import { generateWarningMessage, getComponents } from './utils'
-
-const mockExtensions = {
-  'store/header': {
-    component: 'vtex.LayoutContainer',
-  },
-  'store/home': {
-    component: 'vtex.LayoutContainer',
-    props: {
-      elements: ['carousel', 'shelf'],
-    },
-  },
-  'store/home/carousel': {
-    component: 'vtex.carousel',
-  },
-  'store/home/no-schema': {
-    component: 'vtex.no-schema',
-  },
-  'store/home/shelf': {
-    component: 'vtex.shelf',
-  },
-  'store/home/shelf/arrow': {
-    component: 'vtex.shelf-arrow',
-  },
-  'store/home/shelf/title': {
-    component: 'vtex.shelf-title',
-  },
-}
-
-const mockComponents = {
-  'vtex.carousel': {
-    schema: {
-      title: 'Carousel',
-    },
-  },
-  'vtex.no-schema': {},
-  'vtex.shelf': {
-    schema: {
-      title: 'Shelf',
-    },
-  },
-  'vtex.shelf-arrow': {
-    schema: {
-      title: 'Arrow',
-    },
-  },
-  'vtex.shelf-title': {
-    schema: {
-      title: 'Shelf Title',
-    },
-  },
-}
+import { generateWarningMessage, getComponents, getIsSitewide } from './utils'
 
 describe('getComponents', () => {
+  const mockExtensions = {
+    'store/header': {
+      component: 'vtex.LayoutContainer',
+    },
+    'store/home': {
+      component: 'vtex.LayoutContainer',
+      props: {
+        elements: ['carousel', 'shelf'],
+      },
+    },
+    'store/home/carousel': {
+      component: 'vtex.carousel',
+    },
+    'store/home/no-schema': {
+      component: 'vtex.no-schema',
+    },
+    'store/home/shelf': {
+      component: 'vtex.shelf',
+    },
+    'store/home/shelf/arrow': {
+      component: 'vtex.shelf-arrow',
+    },
+    'store/home/shelf/title': {
+      component: 'vtex.shelf-title',
+    },
+  }
+
+  const mockComponents = {
+    'vtex.carousel': {
+      schema: {
+        title: 'Carousel',
+      },
+    },
+    'vtex.no-schema': {},
+    'vtex.shelf': {
+      schema: {
+        title: 'Shelf',
+      },
+    },
+    'vtex.shelf-arrow': {
+      schema: {
+        title: 'Arrow',
+      },
+    },
+    'vtex.shelf-title': {
+      schema: {
+        title: 'Shelf Title',
+      },
+    },
+  }
+
   it('should filter out components without either a schema or a title', () => {
     expect(
       getComponents(mockExtensions as any, mockComponents as any, 'store/home')
@@ -183,5 +183,84 @@ describe('getComponents', () => {
         generateWarningMessage('vtex.shelf')
       )
     })
+  })
+})
+
+describe('getIsSitewide', () => {
+  const mockExtensions = {
+    'store.home': {
+      after: ['$after_footer'],
+      around: [
+        '$around_homeWrapper',
+        '$around_storeWrapper',
+        '$around_challenge',
+      ],
+      before: ['$before_header.full'],
+      blockId: 'vtex.store-theme@2.x:store.home',
+      blocks: [
+        {
+          blockId: 'vtex.store-theme@2.x:carousel#home',
+          extensionPointId: 'carousel#home',
+        },
+      ],
+      component: 'vtex.render-runtime@8.17.2/LayoutContainer',
+      composition: 'children',
+      content: {},
+      context: {},
+      implementationIndex: 0,
+      implements: [''],
+      preview: { type: 'block' },
+      props: {},
+      render: 'server',
+      track: [],
+    },
+    'store.home/$after_footer': {
+      after: [],
+      around: [],
+      before: [],
+      blockId: 'vtex.store-theme@2.x:footer',
+      blocks: [],
+      component: 'vtex.store-footer@2.6.15/index',
+      content: {},
+      contentMapId: 'gZQaBBQyU2DLvGaM9icNdg',
+      context: {},
+      implementationIndex: 0,
+      implements: [''],
+      preview: null,
+      props: {
+        showPaymentFormsInColor: false,
+        showSocialNetworksInColor: true,
+        showVtexLogoInColor: false,
+      },
+      render: 'server',
+      track: [],
+    },
+    'store.home/carousel#home': {
+      after: [],
+      around: [],
+      before: [],
+      blockId: 'vtex.store-theme@2.x:carousel#home',
+      blocks: [],
+      component: 'vtex.carousel@2.8.0/Carousel',
+      content: {},
+      contentMapId: 'eiYc7wanqAEYiPY5DJdRPT',
+      context: {},
+      implementationIndex: 0,
+      implements: [''],
+      preview: null,
+      props: {},
+      render: 'server',
+      track: [],
+    },
+  }
+
+  it('should return true for AFTER', () => {
+    const mockEditTreePath = 'store.home/$after_footer'
+    expect(getIsSitewide(mockExtensions, mockEditTreePath)).toBe(true)
+  })
+
+  it('should return false for blocks without role', () => {
+    const mockEditTreePath = 'store.home/carousel#home'
+    expect(getIsSitewide(mockExtensions, mockEditTreePath)).toBe(false)
   })
 })
