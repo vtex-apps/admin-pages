6ef5afb589480d9cb604b8e2507334aaa642f804
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-05-02 14:39:26 -0300

    Move Runtime update from config close to quit

--- react/components/EditorContainer/Sidebar/ConfigurationList/index.tsx
+++ react/components/EditorContainer/Sidebar/ConfigurationList/index.tsx
@@ -239,7 +239,7 @@ class ConfigurationList extends Component<Props, State> {
   }
 
   private handleConfigurationClose = () => {
-    const { formMeta, modal } = this.props
+    const { editor, formMeta, iframeRuntime, modal } = this.props
 
     if (formMeta.wasModified) {
       modal.open()
@@ -248,6 +248,10 @@ class ConfigurationList extends Component<Props, State> {
         if (modal.isOpen) {
           modal.close()
         }
+
+        iframeRuntime.updateRuntime({
+          conditions: editor.activeConditions,
+        })
       })
     }
   }
@@ -455,18 +459,12 @@ class ConfigurationList extends Component<Props, State> {
     updateExtensionFromForm(editTreePath, event, intl, iframeRuntime, true)
   }
 
-  private handleQuit = (event?: any) => {
-    const { editor, iframeRuntime } = this.props
-
+  private handleQuit = (event?: Event) => {
     if (event) {
       event.stopPropagation()
     }
 
-    iframeRuntime.updateRuntime({
-      conditions: editor.activeConditions,
-    })
-
-    editor.editExtensionPoint(null)
+    this.props.editor.editExtensionPoint(null)
   }
 
   private isConfigurationDisabled = (configuration: ExtensionConfiguration) => {
