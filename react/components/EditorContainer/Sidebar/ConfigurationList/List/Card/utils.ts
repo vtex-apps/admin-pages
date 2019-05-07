76c8953214d3c49331552e4ec61aded7b6332742
Thiago Murakami <thiago.murakami@gmail.com>
2019-05-07 16:52:10 -0300

    Define messages for configuration tags.

--- react/components/EditorContainer/Sidebar/ConfigurationList/List/Card/utils.ts
+++ react/components/EditorContainer/Sidebar/ConfigurationList/List/Card/utils.ts
@@ -1,4 +1,43 @@
-import { InjectedIntl } from 'react-intl'
+import { defineMessages, InjectedIntl } from 'react-intl'
+
+const messages = defineMessages({
+  brand: {
+    defaultMessage: 'Brand: {id}',
+    id: 'admin/pages.editor.configuration.tag.brand',
+  },
+  category: {
+    defaultMessage: 'Category: {id}',
+    id: 'admin/pages.editor.configuration.tag.category',
+  },
+  department: {
+    defaultMessage: 'Department: {id}',
+    id: 'admin/pages.editor.configuration.tag.department',
+  },
+  product: {
+    defaultMessage: 'Product: {id}',
+    id: 'admin/pages.editor.configuration.tag.product',
+  },
+  route: {
+    defaultMessage: 'URL: {id}',
+    id: 'admin/pages.editor.configuration.tag.route',
+  },
+  search: {
+    defaultMessage: 'Search: {id}',
+    id: 'admin/pages.editor.configuration.tag.search',
+  },
+  sitewide: {
+    defaultMessage: 'Entire site',
+    id: 'admin/pages.editor.configuration.tag.sitewide',
+  },
+  subcategory: {
+    defaultMessage: 'Subcategory: {id}',
+    id: 'admin/pages.editor.configuration.tag.subcategory',
+  },
+  template: {
+    defaultMessage: 'This template',
+    id: 'admin/pages.editor.configuration.tag.template',
+  },
+})
 
 export const getTextFromContext = (
   intl: InjectedIntl,
@@ -7,15 +46,11 @@ export const getTextFromContext = (
   pageContext: PageContext
 ) => {
   if (isSitewide) {
-    return intl.formatMessage({
-      id: 'admin/pages.editor.configuration.tag.sitewide',
-    })
+    return intl.formatMessage(messages.sitewide)
   }
 
   if (pageContext.id === '*' && pageContext.type === '*') {
-    return intl.formatMessage({
-      id: 'admin/pages.editor.configuration.tag.template',
-    })
+    return intl.formatMessage(messages.template)
   }
 
   return intl.formatMessage(
