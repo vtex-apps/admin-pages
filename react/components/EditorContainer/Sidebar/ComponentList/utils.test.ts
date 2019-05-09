9b4ac5a781a19f5ed60077dfbbb42d4a7bb3117d
Thiago Murakami <thiago.murakami@gmail.com>
2019-05-09 11:05:39 -0300

    Change normalize to nest components recursively.

--- react/components/EditorContainer/Sidebar/ComponentList/utils.test.ts
+++ react/components/EditorContainer/Sidebar/ComponentList/utils.test.ts
@@ -28,184 +28,225 @@ describe('normalize', () => {
       { name: 'editor.header.title', treePath: 'store.home/$before_0' },
       {
         name: 'editor.category-menu.title',
-        treePath: 'store.home/$before_0/category-menu'
+        treePath: 'store.home/$before_0/category-menu',
       },
       { name: 'editor.login.title', treePath: 'store.home/$before_0/login' },
       { name: 'editor.menu', treePath: 'store.home/$before_0/menu-link' },
       {
         name: 'editor.minicart.title',
-        treePath: 'store.home/$before_0/minicart'
+        treePath: 'store.home/$before_0/minicart',
       },
       { name: 'editor.carousel.title', treePath: 'store.home/carousel#home' },
-      { name: 'editor.shelf.title', treePath: 'store.home/shelf#home' }
+      { name: 'editor.shelf.title', treePath: 'store.home/shelf#home' },
     ]
 
     const expectedOutput: NormalizedComponent[] = [
       {
         components: [
           {
+            components: [],
             isSortable: false,
             name: 'editor.category-menu.title',
-            treePath: 'store.home/$before_0/category-menu'
+            treePath: 'store.home/$before_0/category-menu',
           },
           {
+            components: [],
             isSortable: false,
             name: 'editor.login.title',
-            treePath: 'store.home/$before_0/login'
+            treePath: 'store.home/$before_0/login',
           },
           {
+            components: [],
             isSortable: false,
             name: 'editor.menu',
-            treePath: 'store.home/$before_0/menu-link'
+            treePath: 'store.home/$before_0/menu-link',
           },
           {
+            components: [],
             isSortable: false,
             name: 'editor.minicart.title',
-            treePath: 'store.home/$before_0/minicart'
-          }
+            treePath: 'store.home/$before_0/minicart',
+          },
         ],
         isSortable: false,
         name: 'editor.header.title',
-        treePath: 'store.home/$before_0'
+        treePath: 'store.home/$before_0',
       },
       {
-        isSortable: true,
+        components: [],
+        isSortable: false,
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel#home'
+        treePath: 'store.home/carousel#home',
       },
       {
-        isSortable: true,
+        components: [],
+        isSortable: false,
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf#home'
+        treePath: 'store.home/shelf#home',
       },
       {
+        components: [],
         isSortable: false,
         name: 'editor.footer.title',
-        treePath: 'store.home/$after_0'
-      }
-    ]
+        treePath: 'store.home/$after_0',
+      },
+    ].map(val => expect.objectContaining(val))
 
-    expect(normalize(input)).toEqual(expectedOutput)
+    // Don't consider order here
+    expect(normalize(input)).toEqual(expect.arrayContaining(expectedOutput))
   })
 
-  it('flattens multilevel components', () => {
+  it('keep multilevel components', () => {
     const input: SidebarComponent[] = [
       {
         name: 'editor.header.title',
-        treePath: 'store.home/$before_0'
+        treePath: 'store.home/$before_0',
+      },
+      {
+        name: 'editor.header.title',
+        treePath: 'store.home/$before_0/menu',
       },
       {
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel#home'
+        treePath: 'store.home/carousel#home',
       },
       {
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf#home'
+        treePath: 'store.home/shelf#home',
       },
       {
         name: 'editor.product-summary.title',
-        treePath: 'store.home/shelf#home/product-summary'
+        treePath: 'store.home/shelf#home/product-summary',
       },
       {
         name: 'editor.product-rating.title',
-        treePath: 'store.home/shelf#home/product-summary/product-rating'
+        treePath: 'store.home/shelf#home/product-summary/product-rating',
+      },
+      {
+        name: 'editor.product-rating.title',
+        treePath: 'store.home/shelf#home/product-summary/product-rating/start',
       },
       {
         name: 'editor.footer.title',
-        treePath: 'store.home/$after_0'
-      }
+        treePath: 'store.home/$after_0',
+      },
     ]
 
     const expectedOutput: NormalizedComponent[] = [
       {
+        components: [
+          {
+            components: [],
+            isSortable: false,
+            name: 'editor.header.title',
+            treePath: 'store.home/$before_0/menu',
+          },
+        ],
         isSortable: false,
         name: 'editor.header.title',
-        treePath: 'store.home/$before_0'
+        treePath: 'store.home/$before_0',
       },
       {
-        isSortable: true,
+        components: [],
+        isSortable: false,
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel#home'
+        treePath: 'store.home/carousel#home',
       },
       {
         components: [
           {
+            components: [
+              {
+                components: [
+                  {
+                    isSortable: false,
+                    name: 'editor.product-rating.title',
+                    treePath:
+                      'store.home/shelf#home/product-summary/product-rating/start',
+                  },
+                ],
+                isSortable: false,
+                name: 'editor.product-rating.title',
+                treePath:
+                  'store.home/shelf#home/product-summary/product-rating',
+              },
+            ],
             isSortable: false,
             name: 'editor.product-summary.title',
-            treePath: 'store.home/shelf#home/product-summary'
+            treePath: 'store.home/shelf#home/product-summary',
           },
-          {
-            isSortable: false,
-            name: 'editor.product-rating.title',
-            treePath: 'store.home/shelf#home/product-summary/product-rating'
-          }
         ],
-        isSortable: true,
+        isSortable: false,
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf#home'
+        treePath: 'store.home/shelf#home',
       },
       {
+        components: [],
         isSortable: false,
         name: 'editor.footer.title',
-        treePath: 'store.home/$after_0'
-      }
+        treePath: 'store.home/$after_0',
+      },
     ]
 
     expect(normalize(input)).toEqual(expectedOutput)
   })
 
-  it('handles unordered components', () => {
+  it.only('handles unordered components', () => {
     const input: SidebarComponent[] = [
       {
         name: 'editor.footer.title',
-        treePath: 'store.home/$after_0'
+        treePath: 'store.home/$after_0',
       },
       {
         name: 'editor.product-summary.title',
-        treePath: 'store.home/shelf#home/product-summary'
+        treePath: 'store.home/shelf#home/product-summary',
       },
       {
         name: 'editor.header.title',
-        treePath: 'store.home/$before_0'
+        treePath: 'store.home/$before_0',
       },
       {
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel#home'
+        treePath: 'store.home/carousel#home',
       },
       {
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf#home'
-      }
+        treePath: 'store.home/shelf#home',
+      },
     ]
 
     const expectedOutput: NormalizedComponent[] = [
       {
+        components: [],
         isSortable: false,
         name: 'editor.header.title',
-        treePath: 'store.home/$before_0'
+        treePath: 'store.home/$before_0',
       },
       {
-        isSortable: true,
+        components: [],
+        isSortable: false,
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel#home'
+        treePath: 'store.home/carousel#home',
       },
       {
         components: [
           {
+            components: [],
             isSortable: false,
             name: 'editor.product-summary.title',
-            treePath: 'store.home/shelf#home/product-summary'
-          }
+            treePath: 'store.home/shelf#home/product-summary',
+          },
         ],
-        isSortable: true,
+        isSortable: false,
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf#home'
+        treePath: 'store.home/shelf#home',
       },
       {
+        components: [],
         isSortable: false,
         name: 'editor.footer.title',
-        treePath: 'store.home/$after_0'
-      }
+        treePath: 'store.home/$after_0',
+      },
     ]
 
     expect(normalize(input)).toEqual(expectedOutput)
@@ -215,72 +256,72 @@ describe('normalize', () => {
     const input: SidebarComponent[] = [
       {
         name: 'editor.header.title',
-        treePath: 'store.home/$before_0'
+        treePath: 'store.home/$before_0',
       },
       {
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel#home'
+        treePath: 'store.home/carousel#home',
       },
       {
         name: 'editor.product-summary.title',
-        treePath: 'store.home/shelf#home/product-summary'
+        treePath: 'store.home/shelf#home/product-summary',
       },
       {
         name: 'editor.around-shelf.title',
-        treePath: 'store.home/shelf#home/$around_0'
+        treePath: 'store.home/shelf#home/$around_0',
       },
       {
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf#home'
+        treePath: 'store.home/shelf#home',
       },
       {
         name: 'editor.around-carousel.title',
-        treePath: 'store.home/carousel#home/$around_0'
+        treePath: 'store.home/carousel#home/$around_0',
       },
       {
         name: 'editor.footer.title',
-        treePath: 'store.home/$after_0'
-      }
+        treePath: 'store.home/$after_0',
+      },
     ]
 
     const expectedOutput: NormalizedComponent[] = [
       {
         isSortable: false,
         name: 'editor.header.title',
-        treePath: 'store.home/$before_0'
+        treePath: 'store.home/$before_0',
       },
       {
         isSortable: false,
         name: 'editor.around-carousel.title',
-        treePath: 'store.home/carousel#home/$around_0'
+        treePath: 'store.home/carousel#home/$around_0',
       },
       {
-        isSortable: true,
+        isSortable: false,
         name: 'editor.carousel.title',
-        treePath: 'store.home/carousel#home'
+        treePath: 'store.home/carousel#home',
       },
       {
         isSortable: false,
         name: 'editor.around-shelf.title',
-        treePath: 'store.home/shelf#home/$around_0'
+        treePath: 'store.home/shelf#home/$around_0',
       },
       {
         components: [
           {
             isSortable: false,
             name: 'editor.product-summary.title',
-            treePath: 'store.home/shelf#home/product-summary'
-          }
+            treePath: 'store.home/shelf#home/product-summary',
+          },
         ],
-        isSortable: true,
+        isSortable: false,
         name: 'editor.shelf.title',
-        treePath: 'store.home/shelf#home'
+        treePath: 'store.home/shelf#home',
       },
       {
         isSortable: false,
         name: 'editor.footer.title',
-        treePath: 'store.home/$after_0'
-      }
+        treePath: 'store.home/$after_0',
+      },
     ]
 
     expect(normalize(input)).toEqual(expectedOutput)
@@ -291,7 +332,7 @@ describe('isRootComponent', () => {
   it(`returns true when called with 'store.home/header.full'`, () => {
     const input: SidebarComponent = {
       name: 'header',
-      treePath: 'store.home/header.full'
+      treePath: 'store.home/header.full',
     }
 
     const expectedOutput = true
@@ -302,7 +343,7 @@ describe('isRootComponent', () => {
   it(`returns true when called with 'store.home/footer'`, () => {
     const input: SidebarComponent = {
       name: 'footer',
-      treePath: 'store.home/footer'
+      treePath: 'store.home/footer',
     }
 
     const expectedOutput = true
@@ -313,7 +354,7 @@ describe('isRootComponent', () => {
   it(`returns true when called with 'store.home/test'`, () => {
     const input: SidebarComponent = {
       name: 'test',
-      treePath: 'store.home/test'
+      treePath: 'store.home/test',
     }
 
     const expectedOutput = true
@@ -324,7 +365,7 @@ describe('isRootComponent', () => {
   it(`returns false when called with 'store.home/header/login'`, () => {
     const input: SidebarComponent = {
       name: 'login',
-      treePath: 'store.home/header.full/login'
+      treePath: 'store.home/header.full/login',
     }
 
     const expectedOutput = false
@@ -335,7 +376,7 @@ describe('isRootComponent', () => {
   it(`returns false when called with 'store.home/shelf/product-summary'`, () => {
     const input: SidebarComponent = {
       name: 'product-summary',
-      treePath: 'store.home/shelf/product-summary'
+      treePath: 'store.home/shelf/product-summary',
     }
 
     const expectedOutput = false
--- react/components/EditorContainer/Sidebar/ComponentList/utils.ts
+++ react/components/EditorContainer/Sidebar/ComponentList/utils.ts
@@ -1,9 +1,11 @@
-import { flatten, partition } from 'ramda'
+import { filter, flatten, partition } from 'ramda'
 
 import { SidebarComponent } from '../typings'
 
 import { getBlockRole } from '../../../../utils/blocks'
 import { NormalizedComponent, NormalizedRelativeRoot } from './typings'
+
+const pathLength = (path: string) => path.split('/').length
 
 export const getParentTreePath = (treePath: string): string => {
   const splitTreePath = treePath.split('/')
@@ -15,97 +17,97 @@ export const getParentTreePath = (treePath: string): string => {
   return splitTreePath.slice(0, splitTreePath.length - 1).join('/')
 }
 
-export const isRootComponent = (component: SidebarComponent) => {
+export const isRootComponent = (minimumTreePathSize: number) => (
+  component: SidebarComponent
+) => {
   const splitTreePath = component.treePath.split('/')
 
   return (
-    splitTreePath.length === 2 ||
-    (splitTreePath.length === 3 && splitTreePath[2].startsWith('$'))
+    splitTreePath.length === minimumTreePathSize ||
+    (splitTreePath.length === minimumTreePathSize + 1 &&
+      splitTreePath[minimumTreePathSize].startsWith('$'))
   )
 }
 
-export const isRootComponentRelative = (rootComponent: SidebarComponent) => {
+export const isRootComponentRelative = (minimumTreePathSize: number) => (
+  rootComponent: SidebarComponent
+) => {
   const splitTreePath = rootComponent.treePath.split('/')
 
   return (
-    splitTreePath[1].startsWith('$') ||
-    (splitTreePath.length === 3 && splitTreePath[2].startsWith('$'))
+    splitTreePath[minimumTreePathSize - 1].startsWith('$') ||
+    (splitTreePath.length === minimumTreePathSize + 1 &&
+      splitTreePath[minimumTreePathSize].startsWith('$'))
   )
 }
 
-export const normalize = (components: SidebarComponent[]) => {
-  const [roots, leaves] = partition(isRootComponent, components)
-
-  const [relativeRoots, nonRelativeRoots] = partition(
-    isRootComponentRelative,
-    roots
+const mountFullTree = (
+  root: SidebarComponent,
+  rootOwnLeaves: SidebarComponent[]
+): NormalizedComponent => {
+  if (rootOwnLeaves.length === 0) {
+    return { ...root, isSortable: false }
+  }
+  const rootTreePathLength = pathLength(root.treePath)
+  const immediateChildren = rootOwnLeaves.filter(
+    ({ treePath: childPath }) =>
+      rootTreePathLength + 1 === pathLength(childPath)
+  )
+  const nonImmediateChildren = rootOwnLeaves.filter(
+    ({ treePath: childPath }) => rootTreePathLength + 1 < pathLength(childPath)
   )
 
-  const normalizedRelativeRoots = relativeRoots.reduce<NormalizedRelativeRoot>(
-    (acc, currRoot) => {
-      const splitTreePath = currRoot.treePath.split('/')
-
-      const treePathTail = splitTreePath[splitTreePath.length - 1]
-
-      const blockRole = getBlockRole(treePathTail)
-
-      if (!blockRole) {
-        return acc
-      }
-
-      const currNormalizedRoot = { ...currRoot, isSortable: false }
-
-      if (blockRole === 'around' && splitTreePath.length === 3) {
-        return {
-          ...acc,
-          nestedAround: [...acc.nestedAround, currNormalizedRoot],
-        }
-      }
-
-      return {
-        ...acc,
-        [blockRole]: [...acc[blockRole], currNormalizedRoot],
-      }
-    },
-    { after: [], around: [], before: [], nestedAround: [] }
-  )
-
-  const normalizedNonRelativeRoots = nonRelativeRoots.map(root => ({
+  return {
     ...root,
+    components: immediateChildren.map(child => {
+      const grandChildren = nonImmediateChildren.filter(({ treePath }) =>
+        treePath.startsWith(child.treePath)
+      )
+      return mountFullTree(child, grandChildren)
+    }),
     isSortable: false,
-  }))
+  }
+}
 
-  const sortedRoots = [
-    ...normalizedRelativeRoots.before,
-    ...normalizedRelativeRoots.around,
-    ...flatten(
-      normalizedNonRelativeRoots.map(nonRelativeRoot => [
-        ...normalizedRelativeRoots.nestedAround.filter(
-          (nestedAround: NormalizedComponent) =>
-            nestedAround.treePath.startsWith(nonRelativeRoot.treePath)
-        ),
-        nonRelativeRoot,
-      ])
-    ),
-    ...normalizedRelativeRoots.after,
-  ]
+export const normalize = (components: SidebarComponent[]) => {
+  const minimumTreePathSize = components.reduce((acc, currentComponent) => {
+    const treePathSize = currentComponent.treePath.split('/').length
+    return treePathSize < acc ? treePathSize : acc
+  }, Infinity)
 
-  return leaves
-    .map(leaf => ({ ...leaf, isSortable: false }))
-    .reduce<NormalizedComponent[]>(
-      (acc, leaf) =>
-        acc.map(root =>
-          leaf.treePath.startsWith(root.treePath)
-            ? {
-                ...root,
-                components: root.components
-                  ? [...root.components, leaf]
-                  : [leaf],
-              }
-            : root
-        ),
-      sortedRoots
+  const [roots, leaves] = partition(
+    isRootComponent(minimumTreePathSize),
+    components
+  )
+
+  const reduceRoot = (
+    acc: SidebarComponent[],
+    root: SidebarComponent
+  ): SidebarComponent[] => {
+    const { treePath: currentRootPath } = root
+    const rootPathLength = pathLength(currentRootPath)
+    const childrenFromRoot = leaves.filter(
+      ({ treePath: leafTreePath }) =>
+        leafTreePath.startsWith(currentRootPath) &&
+        rootPathLength + 1 === pathLength(leafTreePath)
     )
+
+    const rootObject = {
+      ...root,
+      components: childrenFromRoot.map(child => {
+        const grandChildren = leaves.filter(({ treePath }) =>
+          treePath.startsWith(child.treePath)
+        )
+        return mountFullTree(child, grandChildren)
+      }),
+      isSortable: false,
+    }
+    return acc.concat(rootObject)
+  }
+
+  const trees = roots.reduce<SidebarComponent[]>(reduceRoot, [])
+
+  return trees
 }
 
 export const pureSplice = <T>(index: number, target: T[]) => [
