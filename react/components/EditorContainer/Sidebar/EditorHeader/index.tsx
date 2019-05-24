3236c19308e7c613fc3564c1eb4d735fe834b132
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-05-23 21:19:50 -0300

    Standardize the component editor headers

--- react/components/EditorContainer/Sidebar/EditorHeader/index.tsx
+++ react/components/EditorContainer/Sidebar/EditorHeader/index.tsx
@@ -18,18 +18,14 @@ const EditorHeader: React.FunctionComponent<Props> = ({
   shouldDisableSaveButton = false,
   title,
 }) => (
-  <div className={`w-100 ph5 pv4 bb bw1 b--light-silver`}>
+  <div className="w-100 ph5 pv4">
     <div className="w-100 flex justify-between">
       <div className={`flex items-center ${onSave ? 'w-100' : ''}`}>
         <span className="pointer" onClick={onClose}>
           <IconArrowBack size={16} color="#585959" />
         </span>
-        <div className="w-100 pl3 mv1 flex justify-between items-center">
-          {title && (
-            <h4 className="mv0 f6 fw5 dark-gray b--transparent ba bw1">
-              {title}
-            </h4>
-          )}
+        <div className="w-100 pl3 flex justify-between items-center">
+          {title && <h4 className="fw5 ma0 lh-copy f5 near-black">{title}</h4>}
           {onSave && (
             <SaveButton
               isDisabled={shouldDisableSaveButton}
