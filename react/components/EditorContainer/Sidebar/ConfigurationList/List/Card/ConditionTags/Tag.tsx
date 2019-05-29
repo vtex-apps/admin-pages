89e514e9bee9ef1298dedee5898f7a97ca513f7f
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-05-29 18:32:03 -0300

    Replace memoized obj with outer const

--- react/components/EditorContainer/Sidebar/ConfigurationList/List/Card/ConditionTags/Tag.tsx
+++ react/components/EditorContainer/Sidebar/ConfigurationList/List/Card/ConditionTags/Tag.tsx
@@ -10,26 +10,21 @@ interface Props {
   title: string
 }
 
-const Tag: React.FunctionComponent<Props> = ({ kind, text, title }) => {
-  const iconByKind = React.useMemo(
-    () => ({
-      date: <CalendarIcon />,
-      utm: <PersonIcon />,
-    }),
-    []
-  )
-
-  return (
-    <StyleguideTag bgColor="#FFFFFF" color="#727273">
-      <div className="flex items-center">
-        {iconByKind[kind]}
-
-        <span className="mh2 fw5">{title}</span>
-
-        <span className="fw4">{text}</span>
-      </div>
-    </StyleguideTag>
-  )
+const iconByKind = {
+  date: <CalendarIcon />,
+  utm: <PersonIcon />,
 }
+
+const Tag: React.FunctionComponent<Props> = ({ kind, text, title }) => (
+  <StyleguideTag bgColor="#FFFFFF" color="#727273">
+    <div className="flex items-center">
+      {iconByKind[kind]}
+
+      <span className="mh2 fw5">{title}</span>
+
+      <span className="fw4">{text}</span>
+    </div>
+  </StyleguideTag>
+)
 
 export default React.memo(Tag)
