a489bd49575ae306e37c600c2149f8e8d32b089c
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-05 11:33:39 -0300

    Improve rich text editor styles

--- react/components/RichTextEditor.tsx
+++ react/components/RichTextEditor.tsx
@@ -22,7 +22,6 @@ const INLINE_STYLES = [
   {label: 'Bold', style: 'BOLD'},
   {label: 'Italic', style: 'ITALIC'},
   {label: 'Underline', style: 'UNDERLINE'},
-  {label: 'Monospace', style: 'CODE'},
 ]
 
 const BLOCK_TYPES = [
@@ -32,11 +31,79 @@ const BLOCK_TYPES = [
   {label: 'H4', style: 'header-four'},
   {label: 'H5', style: 'header-five'},
   {label: 'H6', style: 'header-six'},
-  {label: 'Blockquote', style: 'blockquote'},
   {label: 'UL', style: 'unordered-list-item'},
   {label: 'OL', style: 'ordered-list-item'},
-  {label: 'Code Block', style: 'code-block'},
 ]
+
+interface StyleBtnProps {
+  label: string | JSX.Element
+  active: boolean
+  onToggle: (style: any) => void
+  style: any
+}
+const StyleButton = ({ active, onToggle, style, label }: StyleBtnProps) => {
+  const handleToggle = (e: any) => {
+    e.preventDefault()
+    onToggle(style)
+  }
+
+  return (
+    <span className={`f6 pointer mr3 ${active ? 'blue b' : ''}`} onMouseDown={handleToggle}>
+      {label}
+    </span>
+  )
+}
+
+const BlockStyleControls = (props: any) => {
+  const { editorState } = props
+  const selection = editorState.getSelection()
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey())
+    .getType()
+
+  return (
+    <div className="mb3">
+      {BLOCK_TYPES.map((type) =>
+        <StyleButton
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      )}
+    </div>
+  )
+}
+
+const InlineStyleControls = (props: any) => {
+  const { editorState } = props
+  const currentStyle = editorState.getCurrentInlineStyle()
+  
+  return (
+    <div className="">
+      {INLINE_STYLES.map((type, i) =>
+        <StyleButton
+          key={i}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      )}
+    </div>
+  )
+}
+
+function getBlockStyle(block: ContentBlock): string {
+  switch (block.getType()) {
+    case 'blockquote': 
+      return styles.RichEditor_blockquote
+    default: 
+      return ''
+  }
+}
 
 const RichTextEditor = () => {
   const [editorState, setEditorState] = React.useState(EditorState.createEmpty())
@@ -48,9 +115,8 @@ const RichTextEditor = () => {
       className += ` ${styles.RichEditor_hidePlaceholder}`
     }
   }
+
   const handleChange = (state: EditorState) => {
-    console.log('----- state:', state)
-    
     return setEditorState(state)
   }
 
@@ -73,9 +139,17 @@ const RichTextEditor = () => {
   }
 
   return (
-    <div className={styles.RichEditor_root}>
-      <BlockStyleControls editorState={editorState} onToggle={toggleBlockType} />
-      <InlineStyleControls editorState={editorState} onToggle={toggleInlineStyle} />
+    <div className="bw1 br2 b--solid b--muted-4">
+      <div className="pa4">
+        <BlockStyleControls
+          editorState={editorState}
+          onToggle={toggleBlockType}
+        />
+        <InlineStyleControls
+          editorState={editorState}
+          onToggle={toggleInlineStyle}
+        />
+      </div>
       <div className={className}>
         <Editor
           editorState={editorState}
@@ -89,68 +163,3 @@ const RichTextEditor = () => {
 }
 
 export default RichTextEditor
-
-const StyleButton = ({ active, onToggle, style, label }: { label: string, active: boolean, onToggle: Function, style: any }) => {
-  const className = `${styles.RichEditor_styleButton} ${active ? styles.RichEditor_activeButton : ''}`
-
-  const handleToggle = (e: any) => {
-    e.preventDefault()
-    onToggle(style)
-  }
-  return (
-    <span className={className} onMouseDown={handleToggle}>
-      {label}
-    </span>
-  )
-}
-
-const BlockStyleControls = (props: any) => {
-  const { editorState } = props
-  const selection = editorState.getSelection()
-  const blockType = editorState
-    .getCurrentContent()
-    .getBlockForKey(selection.getStartKey())
-    .getType()
-
-  return (
-    <div className={styles.RichEditor_controls}>
-      {BLOCK_TYPES.map((type) =>
-        <StyleButton
-          key={type.label}
-          active={type.style === blockType}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  )
-}
-
-const InlineStyleControls = (props: any) => {
-  const { editorState } = props
-  const currentStyle = editorState.getCurrentInlineStyle()
-  
-  return (
-    <div className={styles.RichEditor_controls}>
-      {INLINE_STYLES.map((type) =>
-        <StyleButton
-          key={type.label}
-          active={currentStyle.has(type.style)}
-          label={type.label}
-          onToggle={props.onToggle}
-          style={type.style}
-        />
-      )}
-    </div>
-  )
-}
-
-function getBlockStyle(block: ContentBlock): string {
-  switch (block.getType()) {
-    case 'blockquote': 
-      return styles.RichEditor_blockquote
-    default: 
-      return ''
-  }
-}
