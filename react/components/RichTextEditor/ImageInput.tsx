20720d0d8a3b8675db7f3f2a555b9d2bedaac98c
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-05 15:08:47 -0300

    Improve image input style

--- react/components/RichTextEditor/ImageInput.tsx
+++ react/components/RichTextEditor/ImageInput.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { defineMessages, injectIntl } from 'react-intl'
 
 import { Button, Input } from 'vtex.styleguide'
 
@@ -6,19 +7,35 @@ import StyleButton from './StyleButton'
 
 interface Props {
   onAdd: (link: string) => void
+  intl: any 
 }
-const ImageInput = ({ onAdd }: Props) => {
+
+const messages = defineMessages({
+  btn: {
+    defaultMessage: 'Add',
+    id: 'admin/pages.admin.rich-text-editor.add-image-btn',
+  },
+  label: {
+    defaultMessage: 'Image link',
+    id: 'admin/pages.admin.rich-text-editor.add-image-label',
+  },
+  placeholder: {
+    defaultMessage: 'Link',
+    id: 'admin/pages.admin.rich-text-editor.add-image-placeholder',
+  },
+})
+
+const ImageInput = ({ onAdd, intl }: Props) => {
   const [isOpen, setIsOpen] = React.useState(false)
   const [imageLink, setImageLink] = React.useState()
 
   const handleAddImage = () => {
-    console.log('----- imageLink:', imageLink);
-    
-    onAdd(imageLink)
+    setIsOpen(false)
+    return onAdd(imageLink)
   }
 
   return (
-    <div>
+    <div className="relative">
       <StyleButton 
         active={isOpen}
         onToggle={() => setIsOpen(!isOpen)}
@@ -26,15 +43,21 @@ const ImageInput = ({ onAdd }: Props) => {
         label={'Image'}
       />
 
-      <div>
-        <Input 
-          onChange={(e: any) => setImageLink(e.target.value)}
-        />
-
-        <Button onClick={handleAddImage}>Add</Button>
-      </div>
+      {isOpen && (
+        <div className="flex flex-column absolute pa5 bg-white b--solid b--muted-4 bw1 br2">
+          <div className="mb4">
+            <Input
+              label={intl.formatMessage(messages.label)}
+              onChange={(e: any) => setImageLink(e.target.value)}
+            />
+          </div>
+          <Button onClick={handleAddImage} size="small">
+            { intl.formatMessage(messages.btn) }
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
 
-export default ImageInput
+export default injectIntl(ImageInput)
