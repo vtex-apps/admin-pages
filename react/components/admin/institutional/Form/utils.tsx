83f4157dc6b5328f5fcdfb8f4b6a350dbc9742fe
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-08 19:34:00 -0300

    Add validations

---
+++ react/components/admin/institutional/Form/utils.tsx
@@ -0,0 +1,30 @@
+import { RouteFormData } from 'pages'
+
+import { State } from './index'
+
+const requiredMessage = 'admin/pages.admin.pages.form.templates.field.required'
+
+const validateFalsyPath = (path: keyof RouteFormData) => (
+  data: RouteFormData
+) => !data[path] && { [path]: requiredMessage }
+
+const validateUrlBeginning = (path: string) => {
+  if (path.startsWith('/')) {
+    return {}
+  }
+  return {
+    path: 'admin/pages.admin.pages.form.templates.path.validation-error',
+  }
+}
+
+export const getValidateFormState = (prevState: State) => {
+  return {
+    ...prevState,
+    formErrors: {
+      ...prevState.formErrors,
+      ...(validateUrlBeginning(prevState.data.path) as Record<string, string>),
+      ...validateFalsyPath('path')(prevState.data),
+      ...(prevState.isInfoEditable ? validateFalsyPath('title')(prevState.data) : {}),
+    },
+  }
+}
