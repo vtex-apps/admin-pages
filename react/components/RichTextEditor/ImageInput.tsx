86de75c3de75700e2d45fa072fa1ea442ec6b104
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-15 16:13:07 -0300

    Improve translations

--- react/components/RichTextEditor/ImageInput.tsx
+++ react/components/RichTextEditor/ImageInput.tsx
@@ -24,17 +24,34 @@ interface Props {
 const MAX_SIZE = 4 * 1024 * 1024
 
 const messages = defineMessages({
-  btn: {
+  addBtn: {
     defaultMessage: 'Add',
     id: 'admin/pages.admin.rich-text-editor.add-button',
   },
-  label: {
-    defaultMessage: 'Image link',
+  addLabel: {
+    defaultMessage: 'Image URL',
     id: 'admin/pages.admin.rich-text-editor.add-image-label',
   },
-  placeholder: {
-    defaultMessage: 'Link',
+  addPlaceholder: {
+    defaultMessage: 'URL',
     id: 'admin/pages.admin.rich-text-editor.add-image-placeholder',
+  },
+  fileSizeError: {
+    defaultMessage:
+      'File exceeds the size limit of 4MB. Please choose a smaller one.',
+    id: 'admin/pages.editor.image-uploader.error.file-size',
+  },
+  genericError: {
+    defaultMessage: 'Something went wrong. Please try again.',
+    id: 'admin/pages.editor.image-uploader.error.generic',
+  },
+  uploadBtn: {
+    defaultMessage: 'Upload image',
+    id: 'admin/pages.admin.rich-text-editor.upload-button',
+  },
+  uploadLabel: {
+    defaultMessage: 'Choose an image to upload',
+    id: 'admin/pages.admin.rich-text-editor.upload-image-label',
   },
 })
 
@@ -58,10 +75,10 @@ const ImageInput = ({ onAdd, intl, uploadFile }: Props) => {
 
         return onAdd(fileUrl)
       } else {
-        return setError('Imagem muito grande')
+        return setError(intl.formatMessage(messages.fileSizeError))
       }
     } catch (err) {
-      setError('Error')
+      setError(intl.formatMessage(messages.genericError))
       setIsLoading(false)
     }
   }, [])
@@ -101,25 +118,23 @@ const ImageInput = ({ onAdd, intl, uploadFile }: Props) => {
           <div className={`flex flex-column ${isLoading && 'o-20'}`}>
             <div className="mb4">
               <Input
-                label={intl.formatMessage(messages.label)}
+                label={intl.formatMessage(messages.addLabel)}
                 onChange={(e: any) => setImageUrl(e.target.value)}
-                placeholder="URL da imagem"
+                placeholder={intl.formatMessage(messages.addPlaceholder)}
               />
             </div>
 
             <Button onClick={handleAddImage} size="small">
-              { intl.formatMessage(messages.btn) }
+              { intl.formatMessage(messages.addBtn) }
             </Button>
 
             <SeparatorWithLine />
 
             <div className="flex flex-column">
-              <span className="db mb3 w-100 c-on-base t-small">Upload an image</span>
+              <span className="db mb3 w-100 c-on-base t-small">{intl.formatMessage(messages.uploadLabel)}</span>
               <div { ...getRootProps() } className="flex flex-column">
                 <input { ...getInputProps() } />
-                <Button size="small">
-                  Upload image
-                </Button>
+                <Button size="small">{intl.formatMessage(messages.uploadBtn)}</Button>
               </div>
             </div>
 
