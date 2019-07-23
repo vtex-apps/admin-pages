fffb24b80131e480f0c2245d14b9f10a6f3d4255
Augusto Lazaro <aemlazaro@gmail.com>
2019-07-23 17:15:43 -0300

    Rename routes

--- react/components/admin/pages/consts.ts
+++ react/components/admin/pages/consts.ts
@@ -4,8 +4,8 @@ export const ROUTES_FORM = 'admin.app.cms.pages.page-details'
 
 export const ROUTES_LIST = 'admin.app.cms.pages.page-list'
 
-export const INSTITUTIONAL_ROUTES_LIST = 'admin.app.cms.institutional'
+export const INSTITUTIONAL_ROUTES_LIST = 'admin.app.cms.content'
 
-export const INSTITUTIONAL_ROUTES_FORM = 'admin.app.cms.institutional-details'
+export const INSTITUTIONAL_ROUTES_FORM = 'admin.app.cms.content-details'
 
 export const WRAPPER_PATH = 'pages'
