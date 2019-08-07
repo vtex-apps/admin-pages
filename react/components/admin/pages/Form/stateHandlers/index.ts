53d7de2d75b54c0200d89f4f4d68516a9554fbbb
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-08-07 15:14:42 -0300

    Format code

--- react/components/admin/pages/Form/stateHandlers/index.ts
+++ react/components/admin/pages/Form/stateHandlers/index.ts
@@ -1,7 +1,17 @@
-export { getAddConditionalTemplateState } from './getAddConditionalTemplateState'
-export { getChangeOperatorConditionalTemplateState } from './getChangeOperatorConditionalTemplateState'
-export { getChangeStatementsConditionalTemplate } from './getChangeStatementsConditionalTemplate'
-export { getChangeTemplateConditionalTemplateState } from './getChangeTemplateConditionalTemplateState'
+export {
+  getAddConditionalTemplateState,
+} from './getAddConditionalTemplateState'
+export {
+  getChangeOperatorConditionalTemplateState,
+} from './getChangeOperatorConditionalTemplateState'
+export {
+  getChangeStatementsConditionalTemplate,
+} from './getChangeStatementsConditionalTemplate'
+export {
+  getChangeTemplateConditionalTemplateState,
+} from './getChangeTemplateConditionalTemplateState'
 export { getLoginToggleState } from './getLoginToggleState'
-export { getRemoveConditionalTemplateState } from './getRemoveConditionalTemplateState'
+export {
+  getRemoveConditionalTemplateState,
+} from './getRemoveConditionalTemplateState'
 export { getValidateFormState } from './getValidateFormState'
