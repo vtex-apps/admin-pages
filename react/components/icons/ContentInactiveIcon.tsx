caefeefb5fd39701f503402548795539d0ceed36
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-10-16 17:39:33 -0300

    Create status label to differ active/editing cards

---
+++ react/components/icons/ContentInactiveIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ContentInactiveIcon: React.FC = () => (
+  <svg
+    width="10"
+    height="10"
+    viewBox="0 0 10 10"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <circle cx="5" cy="5" r="5" fill="#CACBCC" />
+  </svg>
+)
+
+export default ContentInactiveIcon
