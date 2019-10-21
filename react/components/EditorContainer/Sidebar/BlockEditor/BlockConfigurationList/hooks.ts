4025b32fd7ee039fdae2a155a2dab7b90893fee7
Felippe Nardi <felippe@nardi.me>
2019-10-21 18:59:28 -0300

    Add modal before deleting configuration

--- react/components/EditorContainer/Sidebar/BlockEditor/BlockConfigurationList/hooks.ts
+++ react/components/EditorContainer/Sidebar/BlockEditor/BlockConfigurationList/hooks.ts
@@ -2,6 +2,7 @@ import { useCallback } from 'react'
 import { defineMessages } from 'react-intl'
 
 import { useEditorContext } from '../../../../EditorContext'
+import { useModalContext } from '../../ModalContext'
 import { getIsDefaultContent } from '../utils'
 
 import { UseListHandlers } from './typings'
@@ -24,15 +25,43 @@ const messages = defineMessages({
     defaultMessage: 'Content reset successfully.',
     id: 'admin/pages.editor.components.content.reset.success',
   },
+  cancel: {
+    defaultMessage: 'Cancel',
+    id: 'admin/pages.editor.components.modal.button.cancel',
+  },
+  delete: {
+    defaultMessage: 'Delete',
+    id: 'admin/pages.editor.components.button.delete',
+  },
+  deleteActiveTitle: {
+    defaultMessage: 'Delete active content',
+    id: 'admin/pages.editor.components.modal.deleteActiveTitle',
+  },
+  deleteActiveText: {
+    defaultMessage:
+      'You are trying to delete an active configuration which will cause another one to take its place. Are you sure you want to delete it?',
+    id: 'admin/pages.editor.components.modal.deleteActiveText',
+  },
+  deleteInactiveTitle: {
+    defaultMessage: 'Delete content',
+    id: 'admin/pages.editor.components.modal.deleteInactiveTitle',
+  },
+  deleteInactiveText: {
+    defaultMessage: 'Are you sure you want to delete this content?',
+    id: 'admin/pages.editor.components.modal.deleteInactiveText',
+  },
 })
 
 export const useListHandlers: UseListHandlers = ({
+  activeContentId,
   deleteContent,
   iframeRuntime,
   intl,
   showToast,
 }) => {
   const editor = useEditorContext()
+
+  const modal = useModalContext()
 
   const handleConfigurationDelete = useCallback(
     async (configuration: ExtensionConfiguration) => {
@@ -92,7 +121,35 @@ export const useListHandlers: UseListHandlers = ({
     [deleteContent, editor, iframeRuntime, intl, showToast]
   )
 
+  const handleConfirmConfigurationDelete = useCallback(
+    (configuration: ExtensionConfiguration) => {
+      const textMessageByType = {
+        active: messages.deleteActiveText,
+        inactive: messages.deleteInactiveText,
+      }
+
+      const configurationType =
+        activeContentId === configuration.contentId ? 'active' : 'inactive'
+
+      modal.open({
+        isActionDanger: true,
+        textButtonAction: intl.formatMessage(messages.delete),
+        textButtonCancel: intl.formatMessage(messages.cancel),
+        textMessage: intl.formatMessage(textMessageByType[configurationType]),
+        actionHandler: async () => {
+          await handleConfigurationDelete(configuration)
+          modal.close()
+        },
+        cancelHandler: () => {
+          modal.close()
+        },
+      })
+    },
+    [modal, handleConfigurationDelete, activeContentId, intl]
+  )
+
   return {
+    handleConfirmConfigurationDelete,
     handleConfigurationDelete,
   }
 }
