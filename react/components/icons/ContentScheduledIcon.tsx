c413c746be444ac1514bbb6d88002937a7a1a700
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2019-10-22 16:29:58 -0300

    Improve ContentScheduledIcon

--- react/components/icons/ContentScheduledIcon.tsx
+++ react/components/icons/ContentScheduledIcon.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ContentScheduledIcon: React.FC = () => (
   <svg
-    width="10"
-    height="10"
+    width="13"
+    height="13"
     viewBox="0 0 10 10"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
@@ -11,20 +11,22 @@ const ContentScheduledIcon: React.FC = () => (
     <path
       d="M5.38477 3.07693L5.38477 5.38463"
       stroke="#134CD8"
-      strokeWidth="1.4"
+      strokeWidth="1"
       strokeMiterlimit="10"
       strokeLinecap="round"
       strokeLinejoin="round"
-    />
+    ></path>
+
     <path
       d="M3.8457 5.38461H5.38416"
       stroke="#134CD8"
-      strokeWidth="1.4"
+      strokeWidth="1"
       strokeMiterlimit="10"
       strokeLinecap="round"
       strokeLinejoin="round"
-    />
-    <circle cx="5" cy="5" r="4.25" stroke="#134CD8" strokeWidth="1.5" />
+    ></path>
+
+    <circle cx="5" cy="5" r="4.25" stroke="#134CD8" strokeWidth="1.5"></circle>
   </svg>
 )
 
