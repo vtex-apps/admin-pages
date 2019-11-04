f94a952b3ce038294ee5c89bf74d54bd0ec48bd8
Felippe Nardi <felippe@nardi.me>
2019-11-04 19:58:21 -0300

    Add icon when hovering editable input

--- react/components/EditorContainer/EditableText/index.tsx
+++ react/components/EditorContainer/EditableText/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IconEdit } from 'vtex.styleguide'
 
 import styles from './styles.css'
 
@@ -20,17 +21,45 @@ const EditableText: React.FC<Props> = ({
   disabled,
   placeholder,
   value,
-}) => (
-  <input
-    className={`truncate outline-0 c-on-base bb bt-0 br-0 bl-0 b--light-gray hover-b--action-primary pb2 font-body input-reset ${baseClassName} ${styles.input}`}
-    onChange={onChange}
-    onBlur={onBlur}
-    disabled={disabled}
-    onKeyPress={onKeyPress}
-    placeholder={placeholder || ''}
-    type="text"
-    value={value || ''}
-  />
-)
+}) => {
+  const [isEditing, setIsEditing] = React.useState(false)
+
+  const handleOnFocus = React.useCallback(() => {
+    setIsEditing(true)
+  }, [setIsEditing])
+
+  const handleOnBlur = React.useCallback(
+    e => {
+      setIsEditing(false)
+      onBlur && onBlur(e)
+    },
+    [setIsEditing]
+  )
+
+  return (
+    <div
+      className={`flex bb b--light-gray ${
+        isEditing ? 'hover-b--action-primary' : ''
+      } pb2 ${isEditing ? 'b--action-primary' : ''} editableTextInputWrapper`}
+    >
+      <input
+        className={`flex-grow-1 w-100 truncate outline-0 c-on-base bn font-body input-reset ${baseClassName} ${styles.input}`}
+        onChange={onChange}
+        onFocus={handleOnFocus}
+        onBlur={handleOnBlur}
+        disabled={disabled}
+        onKeyPress={onKeyPress}
+        placeholder={placeholder || ''}
+        type="text"
+        value={value || ''}
+      />
+      <span
+        className={`editableTextInputWrapper__input ${isEditing ? 'dn' : ''}`}
+      >
+        <IconEdit />
+      </span>
+    </div>
+  )
+}
 
 export default EditableText
