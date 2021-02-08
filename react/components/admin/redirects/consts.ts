9f84f88e92972e7ee58dd8569d208773d2b29827
Victor Raposo <vwraposo@gmail.com>
2021-02-08 13:15:07 -0300

    Updates csv template

--- react/components/admin/redirects/consts.ts
+++ react/components/admin/redirects/consts.ts
@@ -5,13 +5,12 @@ export const getCSVHeader = (hasMultipleBindings: boolean) =>
   hasMultipleBindings ? 'from;to;type;binding;endDate' : 'from;to;type;endDate'
 
 const CSV_SINGLE_BINDING_TEMPLATE = `/temporary;/;TEMPORARY;
-/temporary-with-date;/;TEMPORARY;2025-01-01T00:00:00.000Z
+/temporary-with-date;/;TEMPORARY;2025-01-01T00:00:00.000Z;
 /permanent;/;PERMANENT;
 `
-
 const CSV_MULTIPLE_BINDING_TEMPLATE = `/temporary;/;TEMPORARY;
-/temporary-with-date;/;TEMPORARY;2025-01-01T00:00:00.000Z;123_bindng_id
-/permanent;/;PERMANENT;123_binding_id
+/temporary-with-date;/;TEMPORARY;1234_binding_id;2025-01-01T00:00:00.000Z;
+/permanent;/;PERMANENT;123_binding_id;
 `
 
 export const getCSVTemplate = (hasMultipleBindings: boolean) =>
