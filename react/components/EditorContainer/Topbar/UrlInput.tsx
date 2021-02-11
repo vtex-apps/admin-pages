de9e8134236f8015ca2f3adbf76ce700552c5184
Vitor Gomes <vitorlgomes@hotmail.com>
2021-02-11 20:22:44 -0300

    Format code

--- react/components/EditorContainer/Topbar/UrlInput.tsx
+++ react/components/EditorContainer/Topbar/UrlInput.tsx
@@ -10,9 +10,9 @@ const UrlInput = () => {
 
   const resolveUrlPath = (pathname: string, searchQueries: string) => {
     const searchParams = new URLSearchParams(searchQueries)
-    const blacklistedSearchQueries = ['__siteEditor', '__bindingAddress']
+    const SEARCH_QUERIES_BLACKLIST = ['__siteEditor', '__bindingAddress']
 
-    blacklistedSearchQueries.forEach(query => {
+    SEARCH_QUERIES_BLACKLIST.forEach(query => {
       searchParams.delete(query)
     })
 
