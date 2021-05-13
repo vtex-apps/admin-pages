eb3315dff34445d008e4ebbfd9474c301152e1d0
Bruno Abreu <bruno.abreu@vtex.com.br>
2021-05-13 15:50:04 -0300

    Create a read only widget

--- react/components/admin/store/StoreForm/index.tsx
+++ react/components/admin/store/StoreForm/index.tsx
@@ -29,6 +29,20 @@ interface MutationVariables {
 
 type Props = ChildMutateProps<FormProps, MutationData, MutationVariables>
 
+const ReadOnlyTextWidget = (props: CustomWidgetProps) => {
+  const intl = useIntl()
+
+  if (!props.schema.default) {
+    return null
+  }
+
+  return (
+    <div className="pv3 c-on-base t-small mb3">
+      {formatIOMessage({ id: props.schema.default || '', intl })}
+    </div>
+  )
+}
+
 const CheckboxWidget = (props: CustomWidgetProps) => (
   <div className="pv4">
     <Toggle {...props} />
@@ -43,6 +57,7 @@ const CheckboxWidget = (props: CustomWidgetProps) => (
 const widgets = {
   BaseInput,
   CheckboxWidget,
+  readOnlyText: ReadOnlyTextWidget,
 }
 
 function resolveSchemaForCurrentTab(
