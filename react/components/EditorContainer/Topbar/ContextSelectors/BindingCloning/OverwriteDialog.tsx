5e894238e35635296971a0f73eed26d50da8e556
Filadelfo <filadelfo.braz@gmail.com>
2021-12-01 10:44:32 +0100

    Refactor: Lint OverwriteDialog module

--- react/components/EditorContainer/Topbar/ContextSelectors/BindingCloning/OverwriteDialog.tsx
+++ react/components/EditorContainer/Topbar/ContextSelectors/BindingCloning/OverwriteDialog.tsx
@@ -1,19 +1,22 @@
 import React, { FunctionComponent, useState } from 'react'
 import { Button, Modal } from 'vtex.styleguide'
+
 import { BindingSelectorState } from './BindingSelector'
 
 interface Props {
   isOpen?: boolean
   state: BindingSelectorState
   pathId: string
-  // TODO: better types
-  onClose: () => void,
-  onConfirm?: (...args: any) => any
-  onCancel?: (...args: any) => any
+  onClose: () => void
+  onConfirm?: (value: void | PromiseLike<void>) => void
+  onCancel?: (reason?: unknown) => void
 }
 
 export const useOverwriteDialogState = () => {
-  const [overwriteHandlers, setOverwriteHandlers] = useState<null | { onConfirm: (...args: any) => any, onCancel: (...args: any) => any }>(null)
+  const [overwriteHandlers, setOverwriteHandlers] = useState<null | {
+    onConfirm: (value: void | PromiseLike<void>) => void
+    onCancel: (reason?: unknown) => void
+  }>(null)
 
   return {
     isOverwriteDialogOpen: Boolean(overwriteHandlers),
@@ -21,73 +24,84 @@ export const useOverwriteDialogState = () => {
     hideOverwriteDialog: () => {
       setOverwriteHandlers(null)
     },
-    onOverwriteConfirm: overwriteHandlers?.onConfirm ?? (() => {}),
-    onOverwriteCancel: overwriteHandlers?.onCancel ?? (() => {}),
+    onOverwriteConfirm: overwriteHandlers?.onConfirm ?? (() => undefined),
+    onOverwriteCancel: overwriteHandlers?.onCancel ?? (() => undefined),
   }
 }
 
-const OverwriteDialog: FunctionComponent<Props> = ({ isOpen, onClose, state, pathId, onConfirm, onCancel }) => {
-  const overwrittenContent = state?.filter(item => item.overwrites && item.checked) ?? []
+const OverwriteDialog: FunctionComponent<Props> = ({
+  isOpen,
+  onClose,
+  state,
+  pathId,
+  onConfirm,
+  onCancel,
+}) => {
+  const overwrittenContent =
+    state?.filter(item => item.overwrites && item.checked) ?? []
 
   // TODO: i18n text
   return (
-    <Modal isOpen={isOpen} onClose={onClose} bottomBar={
-      <div className="flex justify-end">
-        <span className="mr4">
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      bottomBar={
+        <div className="flex justify-end">
+          <span className="mr4">
+            <Button
+              variation="secondary"
+              onClick={() => {
+                onCancel?.()
+                onClose()
+              }}
+            >
+              Go back
+            </Button>
+          </span>
           <Button
-            variation="secondary"
+            variation="danger"
             onClick={() => {
-              onCancel?.()
+              onConfirm?.()
               onClose()
-            }}>
-            Go back
+            }}
+          >
+            Overwrite
           </Button>
-        </span>
-        <Button
-          variation="danger"
-          onClick={() => {
-            onConfirm?.()
-            onClose()
-          }}
-        >
-          Overwrite
-        </Button>
-      </div>
-    }>
+        </div>
+      }
+    >
       <div className="mb6">
         <h2>Existing content will be overwritten</h2>
-        {overwrittenContent.length > 1
-          ? (
-            <div>
-              Existing content in these bindings will be permanently deleted:
-            </div>
-          )
-          : (
-            <div>
-              Existing content in this binding will be permanently deleted:
-            </div>
-          )}
-
+        {overwrittenContent.length > 1 ? (
+          <div>
+            Existing content in these bindings will be permanently deleted:
+          </div>
+        ) : (
+          <div>
+            Existing content in this binding will be permanently deleted:
+          </div>
+        )}
         <ul>
-          {overwrittenContent.map((item) => (
-            <li className="mb4">
+          {overwrittenContent.map(item => (
+            <li className="mb4" key={item.id}>
               <div>
-                {item.label}<span className="c-muted-2">{pathId}</span>
+                {item.label}
+                <span className="c-muted-2">{pathId}</span>
               </div>
-
-            {item.supportedLocales && item.supportedLocales.length > 1 && (
-              <ul className="list pa0 ma0 mt2 ml6 f6 c-muted-1">
-                {item.supportedLocales.map((locale, i, arr) => (
-                  <li className="dib">{locale}{(i<arr.length-1) && <span>,{' '}</span>}</li>
-                ))}
-              </ul>
-            )}
+              {item.supportedLocales && item.supportedLocales.length > 1 && (
+                <ul className="list pa0 ma0 mt2 ml6 f6 c-muted-1">
+                  {item.supportedLocales.map((locale, i, arr) => (
+                    <li className="dib" key={locale}>
+                      {locale}
+                      {i < arr.length - 1 && <span>, </span>}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </li>
           ))}
         </ul>
-        <div>
-          Are you sure you want to duplicate the content to them?
-        </div>
+        <div>Are you sure you want to duplicate the content to them?</div>
       </div>
     </Modal>
   )
