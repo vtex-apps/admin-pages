0be07d3e82f81ce6854906b331865ccfef4f8e3e
Christian G <cdavid.ggarcia@gmail.com>
2021-12-03 15:12:51 +0100

    feat: set removeStoreNameTitle
    Co-authored-by: María Pizarro Cuevas <mariapizarro@users.noreply.github.com>

--- react/components/admin/store/StoreForm/index.tsx
+++ react/components/admin/store/StoreForm/index.tsx
@@ -54,7 +54,6 @@ function resolveSchemaForCurrentTab(
     ?.advancedSettings as JSONSchema6) ?? {
     properties: {},
   }
-  console.log('advancedSettingsSchema', advancedSettingsSchema)
 
   switch (tab) {
     case 'general':
