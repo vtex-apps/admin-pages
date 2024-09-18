9f3a16c7d473e439a5fc7cbe0657c2a65ba8f553
Natanael Fernandes Toscano Araujo <36740164+natanfernandes@users.noreply.github.com>
2024-09-18 14:19:17 -0300

    Feat: add new prop to noOptions and set up translations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 
 ## [Unreleased]
 
+### Added
+- Empty message in pages condition template
+
 ## [4.56.1] - 2024-09-12
 
 ## [4.56.0] - 2024-09-09
--- react/typings/vtex.styleguide.d.ts
+++ react/typings/vtex.styleguide.d.ts
@@ -101,6 +101,7 @@ declare module 'vtex.styleguide' {
     statements?: ConditionsStatement[]
     options: Record<string, ConditionsOptions>
     subjectPlaceholder: string
+    noOptionsMessage: string
     isFullWidth?: boolean
     onChangeStatements?: (s: ConditionsStatement[]) => void
     onChangeOperator?: (o: { operator: ConditionsOperator }) => void
